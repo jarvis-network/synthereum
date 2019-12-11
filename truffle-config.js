883d19631c5dd8eed9b6b314c1ff81bf41895a2a
opz <will.shahda@gmail.com>
2019-12-10 22:08:53 -0500

    Turn off mocha test colors

--- truffle-config.js
+++ truffle-config.js
@@ -21,6 +21,9 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  mocha: {
+    useColors: false
+  },
   networks: {
     develop: {
       port: 8545
