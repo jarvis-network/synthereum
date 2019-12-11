478c317fe9de83683ff7e2711b5c315082a40c4f
opz <will.shahda@gmail.com>
2019-12-10 22:06:38 -0500

    Create Rinkeby network truffle configuration

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,21 @@
 const path = require("path");
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+const getEnv = env => {
+  const value = process.env[env];
+
+  if (typeof value === 'undefined') {
+    throw new Error(`${env} environment variable has not been set.`);
+  }
+
+  return value;
+};
+
+const mnemonicEnv = 'ETH_WALLET_MNEMONIC';
+const mnemonic = getEnv(mnemonicEnv);
+
+const rinkebyEnv = 'ETH_RINKEBY_ENDPOINT';
+const rinkebyEndpoint = getEnv(rinkebyEnv);
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -7,6 +24,12 @@ module.exports = {
   networks: {
     develop: {
       port: 8545
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, rinkebyEndpoint);
+      },
+      network_id: 4
     }
   }
 };
