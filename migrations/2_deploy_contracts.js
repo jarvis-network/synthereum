84878d0b2db962e8587c5755f008aff0567220e6
opz <will.shahda@gmail.com>
2019-12-10 22:09:17 -0500

    Update migration to deploy price feed

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var ProvablePriceFeed = artifacts.require("./ProvablePriceFeed.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+  deployer.deploy(ProvablePriceFeed, true);
 };
