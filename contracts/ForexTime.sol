3008d3122d4381530db1128ef7f97ac273154128
opz <will.shahda@gmail.com>
2020-01-25 20:41:31 -0500

    Create base contract for calculating Forex open hours
    Used to prevent child contracts from performing certain operations when
    the Forex is closed.

---
+++ contracts/ForexTime.sol
@@ -0,0 +1,23 @@
+pragma solidity >=0.5.14 <0.7.0;
+
+import "bokkypoobahsdatetimelibrary/contracts/BokkyPooBahsDateTimeContract.sol";
+
+contract ForexTime is BokkyPooBahsDateTimeContract {
+    uint256 constant closeDay = 5;
+    uint256 constant closeHour = 21;
+    uint256 constant openDay = 7;
+    uint256 constant openHour = 17;
+
+    modifier forexOpen() {
+        uint256 dayOfWeek = getDayOfWeek(now);
+        uint256 hour = getHour(now);
+
+        require(
+            dayOfWeek < closeDay
+            || (dayOfWeek == closeDay && hour <= closeHour)
+            || (dayOfWeek == openDay && hour >= openHour),
+            "Market closed for the weekend"
+        );
+        _;
+    }
+}
