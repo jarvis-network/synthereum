1b0817dba152ca3133ed888c1aa484ed79e1d7f5
opz <will.shahda@gmail.com>
2020-01-26 03:42:30 -0500

    Modify the TIC migration to create a derivative for the constructor

--- migrations/3_deploy_tic.js
+++ migrations/3_deploy_tic.js
@@ -1,5 +1,6 @@
 const web3Utils = require("web3-utils");
 var TIC = artifacts.require("TIC");
+var TDCreator = artifacts.require("TokenizedDerivativeCreator");
 var ProvablePriceFeed = artifacts.require("./ProvablePriceFeed.sol");
 
 module.exports = function(deployer, network, accounts) {
@@ -11,39 +12,63 @@ module.exports = function(deployer, network, accounts) {
     .then(latestPrice => {
       const price = latestPrice.price.toString();
 
-      let params = {};
-      let TDCreator = "";
+      let TDCreatorAddr = "";
       let marginCurrency = "";
-      let provider = accounts[0]; // Whoever the liquidity provider should be
+      let returnCalculator = "";
 
-      // kovan fork is used by truffle migration dry-run
-      if (network === "kovan-fork" || network === "kovan") {
-        TDCreator = "0xad7c5516b25661e0A204646b08024cD82ffe6C48";
-        const withdrawLimit = "1000000000000000000000000000000000000";
-        const returnCalculator = "0xad8fD1f418FB860A383c9D4647880af7f043Ef39";
-        // Kovan rDAI address
-        marginCurrency = "0x462303f77a3f17Dbd95eb7bab412FE4937F9B9CB";
+      const supportedMove = web3Utils.toWei("0.2", "ether");
+      const provider = accounts[0]; // Whoever the liquidity provider should be
+      const withdrawLimit = "1000000000000000000000000000000000000";
 
-        params = {
+      let params = {
           priceFeedAddress: ProvablePriceFeed.address,
           defaultPenalty: web3Utils.toWei("1", "ether"),
-          supportedMove: web3Utils.toWei("0.2", "ether"),
+          supportedMove,
           product: identifier,
           fixedYearlyFee: "0",
           withdrawLimit,
           disputeDeposit: web3Utils.toWei("1", "ether"),
-          returnCalculator,
           startingTokenPrice: price,
           expiry: 0, // Temporarily set no expiry
-          marginCurrency,
           returnType: "0", // Linear
           startingUnderlyingPrice: price,
           name: "SynEuro",
           symbol: "synEUR"
-        }
+      };
+
+      if (
+        network === "develop-fork"
+        || network === "develop-fork-fork"
+        || network === "kovan-fork"
+        || network === "kovan"
+      ) {
+        TDCreatorAddr = "0xad7c5516b25661e0A204646b08024cD82ffe6C48";
+        returnCalculator = "0xad8fD1f418FB860A383c9D4647880af7f043Ef39";
+
+        // Kovan rDAI address
+        marginCurrency = "0x462303f77a3f17Dbd95eb7bab412FE4937F9B9CB";
+
+        params.returnCalculator = returnCalculator;
+        params.marginCurrency = marginCurrency;
       }
 
-      return deployer.deploy(TIC, TDCreator, params, marginCurrency, provider);
+      return deployer
+        .then(() => TDCreator.at(TDCreatorAddr))
+        .then(TDCreator => TDCreator.createTokenizedDerivative(params))
+        .then(tx => {
+          const derivativeAddr = tx
+            .logs[tx.logs.length - 1]
+            .args.contractAddress;
+
+          return deployer.deploy(
+            TIC,
+            derivativeAddr,
+            supportedMove,
+            marginCurrency,
+            provider
+          );
+        })
+        .catch(err => console.log(err));
     })
     .catch(err => console.log(err));
 };
