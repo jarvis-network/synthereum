22f16b9f7ad4fa38102cbfc03c6e3ad001505096
opz <will.shahda@gmail.com>
2020-02-21 02:11:11 -0500

    Implement TIC factory for creating multiple SynFiat tokens

---
+++ contracts/TICFactory.sol
@@ -0,0 +1,37 @@
+pragma solidity >=0.5.15 <0.7.0;
+
+pragma experimental ABIEncoderV2;
+
+import {Ownable} from "@openzeppelin/contracts/ownership/Ownable.sol";
+import {TIC} from "./TIC.sol";
+import {TokenizedDerivativeCreator} from "protocol/core/contracts/TokenizedDerivativeCreator.sol";
+
+contract TICFactory is Ownable {
+    TokenizedDerivativeCreator private derivativeCreator;
+
+    // Get a TIC using its token symbol
+    mapping(string => TIC) public symbolToTIC;
+
+    constructor(TokenizedDerivativeCreator _derivativeCreator) public {
+        derivativeCreator = _derivativeCreator;
+    }
+
+    /**
+     * @notice Creates a new TIC
+     * @param params The parameters used to create the underlying derivative
+     * @param liquidityProvider The LP for the TIC
+     */
+    function createTIC(
+        TokenizedDerivativeCreator.Params calldata params,
+        address liquidityProvider
+    )
+        external
+        onlyOwner
+    {
+        symbolToTIC[params.symbol] = new TIC(
+            derivativeCreator,
+            params,
+            liquidityProvider
+        );
+    }
+}
