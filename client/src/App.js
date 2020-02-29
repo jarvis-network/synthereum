a29f0cf764803cb31a1c171d2de41c99458489e1
opz <will.shahda@gmail.com>
2020-02-29 14:41:08 -0500

    Create App grid and background page loader

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { useWeb3Context } from "web3-react";
 
+import { makeStyles } from "@material-ui/core/styles";
 import Backdrop from "@material-ui/core/Backdrop";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles(theme => ({
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: '#fff',
+  },
+}));
 
 export default function App() {
+  const classes = useStyles();
+
   const context = useWeb3Context();
 
   const [loading, setLoading] = useState(true);
@@ -30,6 +42,18 @@ export default function App() {
   } else if (context.error) {
     return <div>Error</div>
   } else {
-    return <></>;
+    return (
+      <div className="App">
+        <Backdrop className={classes.backdrop} open={loading}>
+          <CircularProgress color="inherit" />
+        </Backdrop>
+        <Container maxWidth="sm">
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+            </Grid>
+          </Grid>
+        </Container>
+      </div>
+    );
   }
 }
