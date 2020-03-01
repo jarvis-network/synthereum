45182b6937b688069f7827f09a4665a4c7cfdafb
opz <will.shahda@gmail.com>
2020-03-01 01:27:51 -0500

    Implement component to view all synthetic token exchange rates

---
+++ client/src/ExchangeRates.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { useWeb3Context } from "web3-react";
+
+import { withStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
+import Tooltip from "@material-ui/core/Tooltip";
+
+import LaunchOutlined from "@material-ui/icons/LaunchOutlined";
+
+const EtherscanIcon = withStyles(theme => ({
+  root: {
+    fontSize: "0.8rem",
+  }
+}))(LaunchOutlined);
+
+export default function ExchangeRates(props) {
+  const context = useWeb3Context();
+
+  const { fromWei } = context.library.utils;
+
+  return (
+    <>
+      <Typography variant="h6" gutterBottom>
+        Jarvis Synthetic Tokens
+      </Typography>
+
+      <TableContainer className={props.className} component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Token</TableCell>
+              <TableCell align="right">Exchange Rate</TableCell>
+            </TableRow>
+          </TableHead>
+
+          <TableBody>
+
+            {
+              props.assets.map(asset => (
+                <TableRow key={asset.symbol}>
+                  <TableCell>
+                    {asset.symbol}
+                    &nbsp;
+                    <Tooltip title="View on Etherscan" placement="right">
+                      <Link
+                        href={
+                          asset.derivative &&
+                          `https://kovan.etherscan.io/token/${asset.derivative.options.address}`
+                        }
+                        target="_blank"
+                      >
+                        <EtherscanIcon color="primary" />
+                      </Link>
+                    </Tooltip>
+                  </TableCell>
+                  <TableCell align="right">
+                    {Number(fromWei(asset.price, "ether")).toLocaleString()} DAI
+                  </TableCell>
+                </TableRow>
+              ))
+            }
+
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
+}
