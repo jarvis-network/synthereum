3d24ffd41c340eea9c4d0763bd9c34e10d82204f
opz <will.shahda@gmail.com>
2020-04-07 18:15:50 -0400

    Implement RToken interface with Solidity v0.6.0
    RToken contract was not compatible with Solidity v0.6.0.

---
+++ contracts/IRToken.sol
@@ -0,0 +1,23 @@
+pragma solidity >=0.6.0 <0.7.0;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+abstract contract IRToken is IERC20 {
+    function token() external virtual returns (IERC20);
+
+    function redeemAndTransfer(
+        address redeemTo,
+        uint256 redeemTokens
+    ) external virtual returns (bool);
+
+    function createHat(
+        address[] calldata recipients,
+        uint32[] calldata proportions,
+        bool doChangeHat
+    ) external virtual returns (uint256 hatID);
+
+    function mintWithSelectedHat(
+        uint256 mintAmount,
+        uint256 hatID
+    ) external virtual returns (bool);
+}
