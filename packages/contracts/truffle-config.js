52dd9f149ab75c2687c3826a036514910e61823c
Petar Kirov <petar.p.kirov@gmail.com>
2020-06-08 01:13:14 +0300

    [project setup]: Use Lerna and Yarn workspace to manage the monorepo
    Use Lerna and Yarn's Workspaces feature to manage the project setup and
    to allow the client project to depend on the contracts package.
    
    Also:
    - Ignore `node_modules/` folder recursively in `.gitignore`
    - [contracts/truffle-config.js]: Do not emit ABI files to the contract
      folder, instead rely on lerna and yarn to be able to import one from the
      other.
    - [contracts/package.json]: Add `build` and `build:solidity` scripts

--- packages/contracts/truffle-config.js
+++ packages/contracts/truffle-config.js
@@ -20,7 +20,6 @@ const kovanEndpoint = getEnv(kovanEnv);
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   mocha: {
     useColors: false
   },
