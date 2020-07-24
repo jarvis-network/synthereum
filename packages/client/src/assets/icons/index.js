d88ab5d6ea2a0c1827b8e618752b55f7ef11ec52
Alessandro Bergamaschi <alessandro@jarvis.network>
2020-07-24 10:19:19 +0200

    [client]: Add new synthetic tokens

--- packages/client/src/assets/icons/index.js
+++ packages/client/src/assets/icons/index.js
@@ -3,11 +3,13 @@ import jCHF from "./jCHF.png";
 import jEUR from "./jEUR.png";
 import jGBP from "./jGBP.png";
 import jXAU from "./jXAU.png";
+import jSPX from "./jSPX.png";
 
 export {
     DAI,
     jCHF,
     jEUR,
     jGBP,
-    jXAU
+    jXAU,
+    jSPX
 }
--- packages/client/src/helpers/defaultAssets.js
+++ packages/client/src/helpers/defaultAssets.js
@@ -1,44 +1,54 @@
 const defaultAssets = [
-    {
-      "name": "Jarvis Synthetic Euro",
-      "symbol": "jEUR",
-      "priceFeed": "EURUSD",
-      "contract": null,
-      "derivative": null,
-      "price": "0",
-      "collateral": "0",
-      "totalTokens": "0"
-    },
-    {
-      "name": "Jarvis Synthetic Swiss Franc",
-      "symbol": "jCHF",
-      "priceFeed": "CHFUSD",
-      "contract": null,
-      "derivative": null,
-      "price": "0",
-      "collateral": "0",
-      "totalTokens": "0"
-    },
-    {
-      "name": "Jarvis Synthetic British Pound",
-      "symbol": "jGBP",
-      "priceFeed": "GBPUSD",
-      "contract": null,
-      "derivative": null,
-      "price": "0",
-      "collateral": "0",
-      "totalTokens": "0"
-    },
-    {
-      "name": "Jarvis Synthetic Gold",
-      "symbol": "jXAU",
-      "priceFeed": "XAUUSD",
-      "contract": null,
-      "derivative": null,
-      "price": "0",
-      "collateral": "0",
-      "totalTokens": "0"
-    }
+  {
+    name: "Jarvis Synthetic Euro",
+    symbol: "jEUR",
+    priceFeed: "EURUSD",
+    contract: null,
+    derivative: null,
+    price: "0",
+    collateral: "0",
+    totalTokens: "0",
+  },
+  {
+    name: "Jarvis Synthetic Swiss Franc",
+    symbol: "jCHF",
+    priceFeed: "CHFUSD",
+    contract: null,
+    derivative: null,
+    price: "0",
+    collateral: "0",
+    totalTokens: "0",
+  },
+  {
+    name: "Jarvis Synthetic British Pound",
+    symbol: "jGBP",
+    priceFeed: "GBPUSD",
+    contract: null,
+    derivative: null,
+    price: "0",
+    collateral: "0",
+    totalTokens: "0",
+  },
+  {
+    name: "Jarvis Synthetic Gold",
+    symbol: "jXAU",
+    priceFeed: "XAUUSD",
+    contract: null,
+    derivative: null,
+    price: "0",
+    collateral: "0",
+    totalTokens: "0",
+  },
+  /*{
+    name: "Jarvis Synthetic S&P500",
+    symbol: "jSPX",
+    priceFeed: "SPXUSD",
+    contract: null,
+    derivative: null,
+    price: "0",
+    collateral: "0",
+    totalTokens: "0",
+  },*/
 ];
 
 export default defaultAssets;
