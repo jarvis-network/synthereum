27f76008a20daa7c6e8e22808ce9b3d590aa8816
Alessandro Bergamaschi <alessandro@jarvis.network>
2020-07-30 15:09:10 +0200

    [client]: Fix exchange request icons and amount in Transaction page

--- packages/client/src/components/elements/TransactionTable/index.jsx
+++ packages/client/src/components/elements/TransactionTable/index.jsx
@@ -15,6 +15,7 @@ import useStyles from './styles';
 import { useWeb3Context } from 'web3-react';
 import * as icons from '../../../assets/icons';
 import Loader from '../Loader';
+import { toFixedNumber } from '../../../helpers/utils.js';
 
 const getStatus = (ev, approvedEvents, rejectedEvents, idField) => {
   let status = 'pending';
@@ -74,21 +75,41 @@ const TransactionTable = ({ assets, token }) => {
       if (eventNames[0] === 'MintRequested') {
         toAsset = assets[token].symbol;
         fromAsset = 'DAI';
+        return requestedEvents.map(ev => {
+          return {
+            ...ev,
+            toAsset,
+            fromAsset,
+          };
+        });
       } else if (eventNames[0] === 'RedeemRequested') {
         toAsset = 'DAI';
         fromAsset = assets[token].symbol;
+        return requestedEvents.map(ev => {
+          return {
+            ...ev,
+            toAsset,
+            fromAsset,
+          };
+        });
       } else if (eventNames[0] === 'ExchangeRequested') {
         fromAsset = assets[token].symbol;
-        /// TODO: toAsset using address
+        const toTIC = requestedEvents.map(event => {
+          const ticAddress = event.returnValues.destTIC;
+          const asset = assets.filter(
+            asset => asset.contract.options.address === ticAddress,
+          );
+          return asset[0].symbol;
+        });
+        return requestedEvents.map((ev, index) => {
+          const toAsset = toTIC[index];
+          return {
+            ...ev,
+            toAsset,
+            fromAsset,
+          };
+        });
       }
-
-      return requestedEvents.map(ev => {
-        return {
-          ...ev,
-          toAsset,
-          fromAsset,
-        };
-      });
     } catch (err) {
       console.error(err);
     }
@@ -173,7 +194,7 @@ const TransactionTable = ({ assets, token }) => {
                           height="28"
                           src={icons[ev.fromAsset]}
                         />
-                        {`-${Number(
+                        {`-${toFixedNumber(
                           fromWei(
                             ev.returnValues[
                               ev.event === 'RedeemRequested'
@@ -181,11 +202,25 @@ const TransactionTable = ({ assets, token }) => {
                                 : 'collateralAmount'
                             ],
                           ),
-                        ).toLocaleString()}`}
+                          5,
+                        )}`}
                       </Box>
                     </TableCell>
                   ) : (
-                    <TableCell></TableCell>
+                    <TableCell className={classes.InputAmount}>
+                      <Box component="div" className={classes.AssetCell}>
+                        <img
+                          alt={ev.fromAsset}
+                          width="28"
+                          height="28"
+                          src={icons[ev.fromAsset]}
+                        />
+                        {`-${toFixedNumber(
+                          fromWei(ev.returnValues['numTokens']),
+                          5,
+                        )}`}
+                      </Box>
+                    </TableCell>
                   )}
                   {ev.event === 'MintRequested' ||
                   ev.event === 'RedeemRequested' ? (
@@ -197,7 +232,7 @@ const TransactionTable = ({ assets, token }) => {
                           height="28"
                           src={icons[ev.toAsset]}
                         />
-                        {`+${Number(
+                        {`+${toFixedNumber(
                           fromWei(
                             ev.returnValues[
                               ev.event === 'RedeemRequested'
@@ -205,11 +240,25 @@ const TransactionTable = ({ assets, token }) => {
                                 : 'numTokens'
                             ],
                           ),
-                        ).toLocaleString()}`}
+                          5,
+                        )}`}
                       </Box>
                     </TableCell>
                   ) : (
-                    <TableCell></TableCell>
+                    <TableCell className={classes.OutputAmount}>
+                      <Box component="div" className={classes.AssetCell}>
+                        <img
+                          alt={ev.toAsset}
+                          width="28"
+                          height="28"
+                          src={icons[ev.toAsset]}
+                        />
+                        {`+${toFixedNumber(
+                          fromWei(ev.returnValues['destNumTokens']),
+                          5,
+                        )}`}
+                      </Box>
+                    </TableCell>
                   )}
                   <TableCell align="right">
                     {ev.status && (
