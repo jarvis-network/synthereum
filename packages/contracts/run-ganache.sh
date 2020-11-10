ca4bfeb80ac3cb6ff548d9384a27e0cfe64627d2
Dimitar Nestorov <dimitar.nestorov@jarvis.network>
2020-11-10 18:24:51 +0200

    Adding ganache-cli to dev dependencies

--- packages/contracts/run-ganache.sh
+++ packages/contracts/run-ganache.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Requires ETH_KOVAN_ENDPONT and ETH_WALLET_MNEMONIC env variables
 # - Same variables that are required for the Truffle network configuration
 GAS_LIMIT=8000000
 GAS_PRICE=0x77359400
-ganache-cli -f $ETH_KOVAN_ENDPOINT -d -m $ETH_WALLET_MNEMONIC -i 42 -b 1 -l $GAS_LIMIT --gasPrice $GAS_PRICE "$@"
+
+npx ganache-cli -f $ETH_KOVAN_ENDPOINT -d -m $ETH_WALLET_MNEMONIC -i 42 -b 1 -l $GAS_LIMIT --gasPrice $GAS_PRICE "$@"
