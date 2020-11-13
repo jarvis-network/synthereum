9ac2e9d032f84e7f828982a79fd9a8b5c74ee8f4
Petar Kirov <petar.p.kirov@gmail.com>
2020-11-13 14:07:51 +0200

    [nix]: Add custom yarn nix package definition to use yarn 1.19.1
    Co-authored-by: asim_jarvis <asim@jarvis.network>

--- shell.nix
+++ shell.nix
@@ -1,4 +1,7 @@
 { pkgs ? import <nixpkgs> { } }: with pkgs;
+let
+  yarn = pkgs.callPackage ./nix/pkgs/yarn/default.nix { inherit stdenv; };
+in
 mkShell {
   buildInputs = [
     nodejs-14_x
@@ -7,4 +10,11 @@ mkShell {
     docker
     docker-compose
   ];
+
+  shellHook = ''
+    echo "Welcome to Jarvis"
+    export NODE_PATH=$PWD/.nix-node
+    export NPM_CONFIG_PREFIX=$PWD/.nix-node
+    export PATH=$NODE_PATH/bin:$PATH
+  '';
 }
