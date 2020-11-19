4c8845ab4a16bc0815291a1443bde813a5c39c06
dzek <git-public@dzek.eu>
2020-11-19 09:11:43 +0100

    [Exchange] added ESC key support when searching for pairs

--- packages/client-new/components/exchange/ExchangeCard.tsx
+++ packages/client-new/components/exchange/ExchangeCard.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { CellInfo } from 'react-table';
 
