09fe411eafd8d5a1253cd5447af1702e51ab7695
Petar Kirov <petar.p.kirov@gmail.com>
2020-11-20 05:37:14 +0200

    [web3-utils/base/big-number]: Add additional functionality

--- packages/web3-utils/src/base/big-number.ts
+++ packages/web3-utils/src/base/big-number.ts
@@ -1,6 +1,50 @@
 import BN from 'bn.js';
-import { fromWei } from 'web3-utils';
+import { fromWei, toWei } from 'web3-utils';
+import { mapReduce } from './array-fp-utils';
+import { isObject } from './asserts';
+
+export const zero = new BN(0);
+export const one = new BN(1);
+export const negativeOne = new BN(-1);
+
+export function mapSumBN<T>(array: T[], getter: (elem: T) => BN) {
+  return mapReduce(array, zero, getter, (curr, next) => curr.add(next));
+}
+
+export function sumBN(listOfNumbers: BN[]) {
+  return mapSumBN(listOfNumbers, x => x);
+}
+
+export function weiToNumber(wei: string) {
+  return fromBNToNumber(new BN(wei));
+}
 
 export function fromBNToDecimalString(bn: BN) {
   return fromWei(bn);
+}
+
+export function formatBN(bn: BN) {
+  return fromBNToNumber(bn).toFixed(2);
+}
+
+export function fromBNToNumber(bn: BN) {
+  return Number.parseFloat(fromBNToDecimalString(bn));
+}
+
+export function toBN(str: string) {
+  return new BN(toWei(str.replace(/,/g, '')));
+}
+
+export function replaceBN(obj: unknown) {
+  if (BN.isBN(obj)) {
+    return formatBN(obj);
+  }
+  else if (!isObject(obj)) {
+    return obj;
+  }
+  const result: any = {};
+  for (const [key, value] of Object.entries(obj)) {
+    result[key] = replaceBN(value);
+  }
+  return result;
 }
