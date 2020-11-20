bf1efa0146d311de832553a62095377fae5e7893
Petar Kirov <petar.p.kirov@gmail.com>
2020-11-20 07:24:03 +0200

    [web3-utils/base/iterables]: Add numeric range iterables

---
+++ packages/web3-utils/src/base/iterables.ts
@@ -0,0 +1,29 @@
+import { assert } from './asserts';
+
+export const range = (start: number, end: number, step: number) => {
+  return start < end
+    ? ascendingRange(start, end, step)
+    : descendingRange(start, end, step);
+};
+
+export const ascendingRange = (start: number, end: number, step: number) => {
+  assert(start < end && step > 0);
+  return ({
+    *[Symbol.iterator]() {
+      for (let i = start; i < end; i += step) {
+        yield i;
+      }
+    },
+  });
+};
+
+export const descendingRange = (start: number, end: number, step: number) => {
+  assert(end < start && step > 0);
+  return ({
+    *[Symbol.iterator]() {
+      for (let i = start; end < i; i -= step) {
+        yield i;
+      }
+    },
+  });
+};
