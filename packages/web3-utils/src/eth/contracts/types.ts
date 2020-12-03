3c9a193aa414586d394dcfe93c0a432b8688fa8b
Petar Kirov <petar.p.kirov@gmail.com>
2020-12-03 17:55:26 +0200

    [web3-utils/eth/contracts/types]: Add ContractInfo and TokenInfo types

---
+++ packages/web3-utils/src/eth/contracts/types.ts
@@ -0,0 +1,13 @@
+import { Address } from "../address";
+import { NetworkName } from "../web3-instance";
+import { ERC20 } from "./typechain/ERC20";
+import { BaseContract } from "./typechain/types";
+
+export interface TokenInfo<Net extends NetworkName> extends ContractInfo<Net, ERC20> {
+  decimals: number;
+}
+
+export interface ContractInfo<Net extends NetworkName, Contract extends BaseContract> {
+  address: Address;
+  instance: Contract;
+}
