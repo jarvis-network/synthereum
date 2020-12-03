48d4077dea0f2e9cfd8a07af548cb06a2d659a19
Petar Kirov <petar.p.kirov@gmail.com>
2020-12-03 20:24:28 +0200

    [contracts/examples/intro]: Update example with the latest way to interact with Synthereum

--- packages/contracts/examples/intro/ierc20-tic_factory-tic_interface.ts
+++ packages/contracts/examples/intro/ierc20-tic_factory-tic_interface.ts
@@ -1,92 +1,117 @@
-import BN from 'bn.js';
-import { formatBN } from '@jarvis-network/web3-utils/base/big-number';
+import { AddressOn } from '@jarvis-network/web3-utils/eth/address';
 import {
-  Address,
-  assertIsAddress,
-} from '@jarvis-network/web3-utils/eth/address';
+  formatBN,
+  sumBN,
+  wei,
+} from '@jarvis-network/web3-utils/base/big-number';
 import { getInfuraWeb3 } from '@jarvis-network/web3-utils/apis/infura';
-import { IERC20 } from '../../src/contracts/typechain';
-import { loadRealm } from '../../src/core/load-realm';
+import { allSymbols as allSyntheticAssets } from '../../src/config/data/all-synthetic-asset-symbols';
 import { SupportedNetworkId } from '../../src/config/types';
+import { loadRealm } from '../../src/core/load-realm';
+import { RealmAgent } from '../../src/core/realm-agent';
 
 /**
- * Frontend needs to work with 3 contracts:
- * * IERC20
- * * TICFactory
- * * TICInstance
+ * Apps building on top of Synthereum need to work with a single class - the
+ * Synthereum Realm Agent.
+ *
+ * It implements the following functionality:
+ * - collateralBalance() - get the agent's number of collateral tokens
+ * - syntheticTokenBalanceOf(syntheticToken) - get the agent's number of
+ *   synthetic tokens
+ * - mint(mintWhat, stableCoinCollateral, outputSyntheticTokens) - mint
+ *   sythetic tokens, by providing collateral tokens
+ * - exchange(from, to, stableCoinCollateral, inputSyntheticTokens,
+ *   outputSyntheticTokens) - exchange one type of synthetic token for another
+ * - redeem(redeemWhat, stableCoinCollateral, inputSyntheticTokens) - convert
+ * synthetic tokens back to collateral tokens
  */
 export async function example() {
   // Get Web3 instance (but make sure you're connecting to a supported network):
   const netId: SupportedNetworkId = 42;
   const web3 = getInfuraWeb3(netId);
   const realm = await loadRealm(web3, netId);
+  const myAddress = web3.defaultAccount as AddressOn<typeof netId>;
+  const realmAgent = new RealmAgent(realm, netId, myAddress);
 
-  // Example: IERC20
-  // 1. Get Contract instances from the Synthereum Realm loaded above:
-  const usdcInstance = realm.collateralToken.instance;
-  const jEURInstance = realm.ticInstances.jEUR.syntheticToken.instance;
-
-  // 2. Use this to get the balance of the user
-  const usersUsdcBalance = await getErc20Balance(
-    usdcInstance,
-    assertIsAddress('0x123123'), // TODO use correct address
+  // Example: IERC20 Balances:
+  console.log(
+    `USDC balance: ${formatBN(await realmAgent.collateralBalance())}`,
   );
-  console.log(`USDC balance: ${formatBN(usersUsdcBalance)}`);
+  console.log(
+    `jEUR balance: ${formatBN(
+      await realmAgent.syntheticTokenBalanceOf('jEUR'),
+    )}`,
+  );
 
-  // 3. Use this to get his transactions of the same asset type: USDC <-> USDC
-  // Get past ERC20 transfer events for USDC:
+  /// Get all balances in one promise:
+  function getAllBalances(realmAgent: RealmAgent<'kovan'>) {
+    const usdcBalancePromise = realmAgent.collateralBalance();
+    const allSyntheticTokenBalancePromises = allSyntheticAssets.map(asset =>
+      realmAgent.syntheticTokenBalanceOf(asset),
+    );
+    const allBalances = Promise.all([
+      usdcBalancePromise,
+      ...allSyntheticTokenBalancePromises,
+    ]);
+    return allBalances;
+  }
+
+  // TODO: multiply each balance by its price:
+  const totalBalance = sumBN(await getAllBalances(realmAgent));
+  console.log(`total balance: ${formatBN(totalBalance)}`);
+
+  // TODO: Get past ERC20 transfer events:
   // const allTransferEvents = getAllTransferEvents(usdcInstance, '0x23234');
 
-  // However we don't need to load them manually, as this is handled for us:
-  const jEurTicInstance = realm.ticInstances.jEUR.instance;
-  const jChfTicInstance = realm.ticInstances.jCHF.instance;
-
-  // In the UI we have 3 basic operation in Synthereum:
-  //   * mint()     convert stable coin into j Asset: USDC -> jEUR | USDC -> jGBP
-  //   * exchange() exchange between 2 j Assets: jEUR <-> jGBP
-  //   * redeem()   convert back to stable coin a jAsset: jCHF -> USDC
+  /* ------------------------- Synthereum Realm Agent ------------------------- */
+  /*                                                                            */
+  /* An agent in a Synthereum realm can perform 3 kinds of operations:          */
+  /*  * mint()     convert stable coint to jAsset : USDC -> jEUR | USDC -> jGBP */
+  /*  * exchange() exchange between 2 j Assets: jEUR <-> jGBP                   */
+  /*  * redeem()   convert back to stable coin a jAsset: jCHF -> USDC           */
+  /* -------------------------------------------------------------------------- */
 
   /* --------------------------------- Minting -------------------------------- */
-  const userCollateralInUsdc = 123; // TODO: Use BN
-  const jEurTokensToMint = 567;
-  await jEurTicInstance.methods.mintRequest(
-    userCollateralInUsdc,
-    jEurTokensToMint,
-  );
+  const tx = await realmAgent.mint({
+    outputSynth: 'jEUR',
+    // Mint 1000 wei of jEUR for 1100 wei of USDC | Assuming EUR/USD @ 1.10
+    collateral: wei(1100),
+    outputAmount: wei(1000),
+  });
+  console.log(tx.transactionHash);
+  console.log(tx.events);
+  console.log(tx.blockNumber);
 
-  // /* ------------------------------- Exchanging ------------------------------- */
-  const jChfToEarnFromExchange = 678;
-  jEurTicInstance.methods.exchangeRequest(
-    jChfTicInstance.options.address,
-    jEurTokensToMint / 2,
-    userCollateralInUsdc,
-    jChfToEarnFromExchange,
-  );
+  /**************************************************************************
+   *  Exchanging                                                            *
+   *                                                                        *
+   * Exchange combines redeem and mint in a single transaction.             *
+   * The `collateral` is the amount of USDC which will be redeemed (for the *
+   * input tokens) and then used to mint the output tokens.                 *
+   **************************************************************************/
+  realmAgent.exchange({
+    // Redeem jEUR for USDC | Assuming EUR/USD is 1.10
+    inputSynth: 'jEUR',
+    inputAmount: wei(100),
+    collateral: wei(110),
+
+    // Mint jGBP with USDC  | Assuming GBP/USD is 1.35
+    outputAmount: wei(148),
+    outputSynth: 'jGBP',
+  });
 
   /* -------------------------------- Redeeming ------------------------------- */
-  const howMuchUsdcToRedeem = 234;
-
-  jEurTicInstance.methods.redeemRequest(
-    howMuchUsdcToRedeem,
-    jChfToEarnFromExchange / 3,
-  );
+  realmAgent.redeem({
+    inputSynth: 'jGBP',
+    // Redeem USDC from jGBP  | Assuming GBP/USD is 1.35
+    collateral: wei(135),
+    inputAmount: wei(100),
+  });
 
   // Wait for approval...
+  // TODO: check if validator approved the request (if meta-sigs are not used)
+
   // Update the user's balance:
-
-  const usersjEURBalance = await getErc20Balance(
-    jEURInstance,
-    assertIsAddress('0x123123'),
-  );
-
-  const uiText = `The user's USDC balance is ${formatBN(usersjEURBalance)}`;
-  return uiText;
-}
-
-export async function getErc20Balance(
-  contract: IERC20,
-  address: Address,
-): Promise<BN> {
-  const balance = await contract.methods.balanceOf(address).call();
-  return new BN(balance);
+  const usersjEURBalance = await realmAgent.syntheticTokenBalanceOf('jEUR');
+  return `The user's USDC balance is ${formatBN(usersjEURBalance)}`;
 }
