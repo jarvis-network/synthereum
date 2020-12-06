b6110171d0ac53aa7740ae5ce57348045ae29dfb
Petar Kirov <petar.p.kirov@gmail.com>
2020-12-06 23:07:41 +0200

    [web3-utils/base/meta]: Add identity type - `Id<T>`

--- packages/web3-utils/src/base/meta.ts
+++ packages/web3-utils/src/base/meta.ts
@@ -4,6 +4,22 @@ export const t = <T extends any[]>(...args: T): T => args;
 export type KeysOf<T> = keyof T;
 export type ValuesOf<T> = T[KeysOf<T>];
 
+/**
+ * Gets the object representation of a type. Useful for "flattening" interesction types.
+ *
+ * Example:
+ * ```ts
+ * type A = { a: 1; b: 2; };
+ * type B = { c: 3; };
+ * type Merged = Id<A & B>;
+ * ```
+ * Becomes:
+ * ```ts
+ * type Merged = { a: 1; b: 2; c: 3; }
+ * ```
+ */
+export type Id<T> = {} & { [K in keyof T]: T[K] };
+
 export type KeyFromValue<V, T extends Record<PropertyKey, PropertyKey>> = {
   [K in KeysOf<T>]: V extends T[K] ? K : never;
 }[keyof T];
