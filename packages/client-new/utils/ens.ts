66d73ba31ea8a7c5576ae439bd67aace8a723e08
Petar Kirov <petar.p.kirov@gmail.com>
2020-12-09 16:10:02 +0200

    [client-new/utils/{ens,usePrettyName}]: Fix TS issues

--- packages/client-new/utils/ens.ts
+++ packages/client-new/utils/ens.ts
@@ -1,5 +1,12 @@
 import Web3 from 'web3';
-import namehash from 'eth-ens-namehash';
+import { assertNotNull } from '@jarvis-network/web3-utils/base/asserts';
+import {
+  Address,
+  assertIsAddress,
+} from '@jarvis-network/web3-utils/eth/address';
+import { getOrCreateElementAsync } from '@jarvis-network/web3-utils/base/optional';
+
+const namehash = require('eth-ens-namehash');
 
 export class ENSHelper {
   private web3: Web3;
@@ -10,40 +17,24 @@ export class ENSHelper {
     this.web3 = web3;
   }
 
-  reverse = async (address: string) => {
-    if (!address) {
-      return null;
-    }
+  reverse = async (address: Address) => {
     const addr = `${address.substr(2).toLowerCase()}.addr.reverse`;
     const hash = namehash.hash(addr);
 
     const contract = await this.web3.eth.ens.getResolver(addr);
-    const name = await contract.methods.name(hash).call();
-    return name ? String(name) : null;
+    return assertIsAddress(await contract.methods.name(hash).call());
   };
 
-  prettyName = async (address: string) => {
-    if (this.prettyCache.has(address)) {
-      return this.prettyCache.get(address);
-    }
-
-    let reverseName: string;
-    let ownerAddress: string;
-
-    try {
-      reverseName = await this.reverse(address);
-      ownerAddress = await this.web3.eth.ens.getOwner(reverseName);
-    } catch (error) {
-      reverseName = null;
-      ownerAddress = null;
-    }
-
-    if (ownerAddress && ownerAddress.toLowerCase() === address) {
-      this.prettyCache.set(address, reverseName);
-    } else {
-      this.prettyCache.set(address, null);
-    }
-
-    return this.prettyCache.get(address);
+  prettyName = async (address: Address): Promise<string> => {
+    return getOrCreateElementAsync(this.prettyCache, address, async () => {
+      const reverseName = await this.reverse(address);
+      const ownerAddress = assertNotNull(
+        await this.web3.eth.ens.getOwner(reverseName),
+      );
+      if (ownerAddress && ownerAddress.toLowerCase() === address) {
+        this.prettyCache.set(address, reverseName);
+      }
+      return reverseName;
+    });
   };
 }
--- packages/client-new/utils/usePrettyName.ts
+++ packages/client-new/utils/usePrettyName.ts
@@ -1,15 +1,16 @@
 import { useContext, useEffect, useState } from 'react';
 
 import { ENSContext } from '@/components/auth/AuthProvider';
+import { Address } from '@jarvis-network/web3-utils/eth/address';
 
 const noop = () => undefined;
 
-export const usePrettyName = (address: string) => {
-  const [name, setName] = useState<string>(null);
+export const usePrettyName = (address: Address | null) => {
+  const [name, setName] = useState<string | null>(null);
   const ens = useContext(ENSContext);
 
   useEffect(() => {
-    if (!ens) {
+    if (!ens || !address) {
       return () => {};
     }
 
