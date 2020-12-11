f59657e6e7eb2d6e3584b2a5cc0e60cbb9525619
Petar Kirov <petar.p.kirov@gmail.com>
2020-12-11 18:01:42 +0200

    [contracts/config/supported-networks]: Add `parseSupportedNetworkId` function

--- packages/contracts/src/config/supported-networks.ts
+++ packages/contracts/src/config/supported-networks.ts
@@ -1,6 +1,24 @@
-import { typeCheck } from "@jarvis-network/web3-utils/base/meta";
-import { NetworkId, ToNetworkName } from "@jarvis-network/web3-utils/eth/networks";
+import {
+  parseInteger,
+  throwError,
+} from '@jarvis-network/web3-utils/base/asserts';
+import { typeCheck } from '@jarvis-network/web3-utils/base/meta';
+import {
+  NetworkId,
+  ToNetworkName,
+} from '@jarvis-network/web3-utils/eth/networks';
 
 export const supportedNetworkIds = typeCheck<NetworkId[]>()([42] as const);
 export type SupportedNetworkId = typeof supportedNetworkIds[number];
 export type SupportedNetworkName = ToNetworkName<SupportedNetworkId>;
+
+export function parseSupportedNetworkId(x: unknown): SupportedNetworkId {
+  const id = parseInteger(x);
+  const supported = supportedNetworkIds as readonly number[];
+  return supported.findIndex(s => id === s) !== -1
+    ? id as SupportedNetworkId
+    : throwError(
+        `${x} is not a supported networkId. Supported network ids are: ` +
+          `[${supportedNetworkIds}]`,
+      );
+}
