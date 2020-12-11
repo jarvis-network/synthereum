abaa01264b7fc0fd0fbe80c4d2c59d96db689d8a
Petar Kirov <petar.p.kirov@gmail.com>
2020-12-11 18:52:25 +0200

    [web3-utils/eth/web3-instance]: Fix `NetworkOf` implementation after the changes to `ValueOnNetwork` and `tagged-type`

--- packages/web3-utils/src/eth/web3-instance.ts
+++ packages/web3-utils/src/eth/web3-instance.ts
@@ -1,7 +1,7 @@
 import type Web3 from 'web3';
 import type { Network, NetworkName, ValueOnNetwork } from './networks';
-import type { TagOf } from '../base/tagged-type';
+import type { TagKindOf } from '../base/tagged-type';
 
 export type Web3On<Net extends Network> = ValueOnNetwork<Web3, Net>;
-export type NetworkOf<Web3 extends Web3On<NetworkName>> = TagOf<Web3>;
+export type NetworkOf<Web3 extends Web3On<NetworkName>> = TagKindOf<Web3>['network'];
 export type { NetworkName };
