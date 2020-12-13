8e4144cdd4dea0423ad5b4bd964d88c620dd96c7
Petar Kirov <petar.p.kirov@gmail.com>
2020-12-13 13:41:05 +0200

    [web3-utils]: Delete Web3Service and move the only useful code - `setPrivateKey` - to `web3-instance`

--- packages/web3-utils/src/eth/web3-instance.ts
+++ packages/web3-utils/src/eth/web3-instance.ts
@@ -5,3 +5,18 @@ import type { TagKindOf } from '../base/tagged-type';
 export type Web3On<Net extends Network> = ValueOnNetwork<Web3, Net>;
 export type NetworkOf<Web3 extends Web3On<NetworkName>> = TagKindOf<Web3>['network'];
 export type { NetworkName };
+
+/**
+ * Sets the default account on a web3 instance to a new one specified as a
+ * private key.
+ *
+ * This function **should only be used for development purposes**.
+ *
+ * @param web3 Web3 instance to set the account on
+ * @param privateKey hex-encoded secp256k1 private key
+ */
+export function setPrivateKey_DevelopmentOnly(web3: Web3, privateKey: string) {
+  const account = web3.eth.accounts.privateKeyToAccount(privateKey);
+  web3.eth.accounts.wallet.add(account);
+  web3.eth.defaultAccount = account.address;
+}
