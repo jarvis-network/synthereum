d505d3f7df7b3528a6e607c1d46ea93c3cbc648e
asim_jarvis <asim@jarvis.network>
2020-12-18 20:39:12 +0500

    feat(contracts): :sparkles: Add assertion for the synthetic symbols

--- libs/contracts/src/config/data/all-synthetic-asset-symbols.ts
+++ libs/contracts/src/config/data/all-synthetic-asset-symbols.ts
@@ -1,3 +1,5 @@
+import { assert, isString } from '@jarvis-network/web3-utils/base/asserts';
+
 export const allSymbols = [
   'jEUR',
   'jGBP',
@@ -10,3 +12,12 @@ export const allSymbols = [
 
 export type SyntheticSymbols = typeof allSymbols;
 export type SyntheticSymbol = SyntheticSymbols[number];
+
+export function isSyntheticSymbol(x: unknown): x is SyntheticSymbol {
+  return isString(x) && allSymbols.indexOf(x as SyntheticSymbol) > -1;
+}
+
+export function assertIsSyntheticSymbol(x: unknown): SyntheticSymbol {
+  assert(isSyntheticSymbol(x));
+  return x as SyntheticSymbol;
+}
