80626da229ea0738aac1ccd6c032b7c23abb8618
asim_jarvis <asim@jarvis.network>
2020-12-18 11:32:37 +0500

    feat(validator): :recycle: Use validator-lib services and config

--- apps/validator/src/config.ts
+++ apps/validator/src/config.ts
@@ -1,31 +1,26 @@
 import {
+  parseSupportedNetworkId,
+  SupportedNetworkId,
+} from '@jarvis-network/synthereum-contracts/dist/src/config/supported-networks';
+import {
+  env as validatorLibenv,
+  ENV as validatorLibENV,
+} from '@jarvis-network/validator-lib';
+import {
   assertIsString,
-  parseFiniteFloat,
   parseInteger,
 } from '@jarvis-network/web3-utils/base/asserts';
-import { parseSupportedNetworkId } from '@jarvis-network/synthereum-contracts/dist/src/config/supported-networks';
-import { SupportedNetworkId } from '@jarvis-network/synthereum-contracts/dist/src/config/supported-networks';
+const { FREQUENCY, PRIVATE_KEY, NETWORK_ID } = process.env;
 
-const {
-  FREQUENCY,
-  PRIVATE_KEY,
-  MAX_SLIPPAGE,
-  LOG_LEVEL,
-  NETWORK_ID,
-} = process.env;
-
-export interface ENV {
+export interface ENV extends validatorLibENV {
   FREQUENCY: number;
   PRIVATE_KEY: string;
-  MAX_SLIPPAGE: number;
-  LOG_LEVEL: string;
   NETWORK_ID: SupportedNetworkId;
 }
 
 export const env: ENV = {
   FREQUENCY: parseInteger(FREQUENCY),
   PRIVATE_KEY: assertIsString(PRIVATE_KEY),
-  MAX_SLIPPAGE: parseFiniteFloat(MAX_SLIPPAGE),
-  LOG_LEVEL: assertIsString(LOG_LEVEL),
   NETWORK_ID: parseSupportedNetworkId(NETWORK_ID),
+  ...validatorLibenv,
 };
