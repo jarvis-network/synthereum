88c8c0669737b8bc0ec7152fcab6f3a456b18146
asim_jarvis <asim@jarvis.network>
2020-12-18 11:32:37 +0500

    feat(validator): :recycle: Use validator-lib services and config

--- apps/validator/src/services/SynFiatKeeper.ts
+++ apps/validator/src/services/SynFiatKeeper.ts
@@ -1,27 +1,18 @@
-import { Logger } from '../logger';
-import * as bunyan from 'bunyan';
-import { performance } from 'perf_hooks';
-import type { ENV } from '../config';
-import { NonPayableTransactionObject } from '@jarvis-network/web3-utils';
-
-import { scale } from '@jarvis-network/web3-utils/base/big-number';
-import { getPriceFeedOhlc } from '../api/jarvis_market_price_feed';
-import { base } from '@jarvis-network/web3-utils';
 import { SupportedNetworkName } from '@jarvis-network/synthereum-contracts/dist/src/config/supported-networks';
 import {
   SynthereumPool,
   SynthereumRealmWithWeb3,
 } from '@jarvis-network/synthereum-contracts/dist/src/core/types';
 import {
-  AddressOn,
-  assertIsAddress,
-} from '@jarvis-network/web3-utils/eth/address';
-import {
-  getTokenAllowance,
-  getTokenBalance,
-  scaleTokenAmountToWei,
-} from '@jarvis-network/web3-utils/eth/contracts/erc20';
-
+  createEverLogger,
+  ExchangeRequestService,
+  MintRequestService,
+  RedeemRequestService,
+} from '@jarvis-network/validator-lib';
+import { base, NonPayableTransactionObject } from '@jarvis-network/web3-utils';
+import { AddressOn } from '@jarvis-network/web3-utils/eth/address';
+import { performance } from 'perf_hooks';
+import { ENV } from '../config';
 type ApproveRejectMethod = (
   id: string | number[],
 ) => NonPayableTransactionObject<void>;
@@ -39,14 +30,27 @@ export default class SynFiatKeeper<Net extends SupportedNetworkName> {
   interval?: ReturnType<typeof setInterval>;
   maxSlippage: number;
   frequency: number;
-
+  exchangeService: ExchangeRequestService<Net>;
+  redeemService: RedeemRequestService<Net>;
+  mintService: MintRequestService<Net>;
+  private logger = createEverLogger({
+    name: 'validator',
+  });
   constructor(
     private readonly realm: SynthereumRealmWithWeb3<Net>,
     { FREQUENCY, MAX_SLIPPAGE }: ENV,
-    private readonly logger: bunyan = Logger('KeeperBot', 'logging', 'info'),
   ) {
     this.frequency = FREQUENCY;
     this.maxSlippage = MAX_SLIPPAGE;
+    this.exchangeService = new ExchangeRequestService(this.realm, {
+      MAX_SLIPPAGE,
+    } as ENV);
+    this.redeemService = new RedeemRequestService({
+      MAX_SLIPPAGE,
+    } as ENV);
+    this.mintService = new MintRequestService({
+      MAX_SLIPPAGE,
+    } as ENV);
   }
 
   get defaultAccount(): AddressOn<Net> {
@@ -85,11 +89,7 @@ export default class SynFiatKeeper<Net extends SupportedNetworkName> {
     approveRequestMethod: ApproveRejectMethod,
     rejectRequestMethod: ApproveRejectMethod,
     type: 'mint' | 'redeem' | 'exchange', // Used for logging
-    callback: (
-      request: T,
-      price: number,
-      requestTime: string,
-    ) => Promise<boolean>,
+    callback: (request: T) => Promise<boolean>,
   ) {
     const requests = await getRequestsMethod().call({
       from: this.defaultAccount,
@@ -100,20 +100,7 @@ export default class SynFiatKeeper<Net extends SupportedNetworkName> {
     );
 
     for (const request of requests) {
-      const { priceFeed } = info;
-      const requestTime = request[1];
-      const price = await getPriceFeedOhlc(priceFeed, requestTime);
-
-      if (!price) {
-        this.logger.info('Forex is closed');
-        continue;
-      }
-
-      this.logger.info(
-        `${info.symbol} was ${price} for ${type} request ${request[0]}`,
-      );
-
-      const approve = await callback(request, price, requestTime);
+      const approve = await callback(request);
 
       await this.finishRequest(
         request[0],
@@ -130,50 +117,14 @@ export default class SynFiatKeeper<Net extends SupportedNetworkName> {
       info.instance.methods.approveMint,
       info.instance.methods.rejectMint,
       'mint',
-      async (request, price) => {
-        const collateral = scaleTokenAmountToWei({
-          amount: request[3][0],
-          decimals: info.collateralToken.decimals,
+      async request => {
+        return this.mintService.CheckRequest(info, {
+          mint_id: request[0],
+          timestamp: request[1],
+          sender: request[2],
+          collateral_amount: request[3],
+          num_tokens: request[4],
         });
-        const tokens = scaleTokenAmountToWei({
-          amount: request[4][0],
-          decimals: info.syntheticToken.decimals,
-        });
-        // const tokens = parseTokens(request[4][0], info.syntheticToken.decimals);
-
-        this.logger.info(
-          `Minting ${tokens} tokens with ${collateral} collateral`,
-        );
-
-        if (collateral.lt(scale(tokens, price * (1 - this.maxSlippage)))) {
-          this.logger.info(`Mint request ${request[0]} is undercollateralized`);
-          return false;
-        }
-
-        const sender = assertIsAddress<Net>(request[2]);
-        const allowance = await getTokenAllowance(
-          info.collateralToken,
-          sender,
-          info.address,
-        );
-        const balance = await getTokenBalance(info.collateralToken, sender);
-
-        if (balance < collateral) {
-          this.logger.info(
-            `Mint request ${request[0]} is not covered by user's collateral balance`,
-          );
-          return false;
-        }
-
-        if (allowance < collateral) {
-          this.logger.info(
-            `Unable to approve mint request ${request[0]} until TIC is given an allowance to transfer the user's collateral`,
-          );
-
-          return false;
-        }
-
-        return true;
       },
     );
   }
@@ -185,50 +136,14 @@ export default class SynFiatKeeper<Net extends SupportedNetworkName> {
       info.instance.methods.approveRedeem,
       info.instance.methods.rejectRedeem,
       'redeem',
-      async (request, price) => {
-        const collateral = scaleTokenAmountToWei({
-          amount: request[3][0],
-          decimals: info.collateralToken.decimals,
+      async request => {
+        return this.redeemService.CheckRequest(info, {
+          redeem_id: request[0],
+          timestamp: request[1],
+          sender: request[2],
+          collateral_amount: request[3],
+          num_tokens: request[4],
         });
-        const tokens = scaleTokenAmountToWei({
-          amount: request[4][0],
-          decimals: info.syntheticToken.decimals,
-        });
-
-        this.logger.info(
-          `Redeeming ${tokens} tokens with ${collateral} collateral`,
-        );
-
-        if (collateral > scale(tokens, price * (1 + this.maxSlippage))) {
-          this.logger.info(
-            `Redeem request ${request[0]} is undercollateralized`,
-          );
-          return false;
-        }
-
-        const sender = assertIsAddress<Net>(request[2]);
-        const allowance = await getTokenAllowance(
-          info.syntheticToken,
-          sender,
-          info.address,
-        );
-        const balance = await getTokenBalance(info.syntheticToken, sender);
-
-        if (balance.lt(tokens)) {
-          this.logger.info(
-            `Redeem request ${request[0]} is not covered by user's ${info.symbol} balance`,
-          );
-          return false;
-        }
-
-        if (allowance.lt(tokens)) {
-          this.logger.info(
-            `Unable to approve redeem request ${request[0]} until TIC is given an allowance to transfer the user's collateral`,
-          );
-          return false;
-        }
-
-        return true;
       },
     );
   }
@@ -240,73 +155,16 @@ export default class SynFiatKeeper<Net extends SupportedNetworkName> {
       info.instance.methods.approveExchange,
       info.instance.methods.rejectExchange,
       'exchange',
-      async (request, price, requestTime) => {
-        const destTic = request[3];
-        const destinationInfo = Object.values(this.realm.ticInstances).find(
-          pool => pool.address === destTic,
-        );
-        if (!destinationInfo) {
-          this.logger.warn(`No TIC configured for address ${destTic}`);
-          return false;
-        }
-
-        const { priceFeed: destinationPriceFeed, symbol } = destinationInfo;
-        const destPrice = await getPriceFeedOhlc(
-          destinationPriceFeed,
-          requestTime,
-        );
-
-        if (!destPrice) {
-          this.logger.info('Forex is closed');
-          return false;
-        }
-
-        this.logger.info(
-          `${symbol} was ${destPrice} for exchange request ${request[0]}`,
-        );
-
-        const tokens = scaleTokenAmountToWei({
-          amount: request[4][0],
-          decimals: info.syntheticToken.decimals,
+      async request => {
+        return this.exchangeService.CheckRequest(info, {
+          exchange_id: request[0],
+          timestamp: request[1],
+          sender: request[2],
+          dest_tic: request[3],
+          num_tokens: request[4],
+          collateral_amount: request[5],
+          dest_num_tokens: request[6],
         });
-        const destTokens = scaleTokenAmountToWei({
-          amount: request[5][0],
-          decimals: info.syntheticToken.decimals,
-        });
-
-        if (
-          scale(tokens, price) <
-          scale(destTokens, destPrice * (1 - this.maxSlippage))
-        ) {
-          this.logger.info(
-            `Exchange request ${request[0]} transfers too many destination tokens`,
-          );
-          return false;
-        }
-
-        const sender = assertIsAddress<Net>(request[2]);
-        const allowance = await getTokenAllowance(
-          info.syntheticToken,
-          sender,
-          info.address,
-        );
-        const balance = await getTokenBalance(info.syntheticToken, sender);
-
-        if (balance < tokens) {
-          this.logger.info(
-            `Exchange request ${request[0]} is not covered by user's ${info.symbol} balance`,
-          );
-          return false;
-        }
-
-        if (allowance < tokens) {
-          this.logger.info(
-            `Unable to approve exchange request ${request[0]} until TIC is given an allowance to transfer the user's tokens`,
-          );
-          return false;
-        }
-
-        return true;
       },
     );
   }
@@ -318,10 +176,8 @@ export default class SynFiatKeeper<Net extends SupportedNetworkName> {
   ) {
     try {
       const from = this.defaultAccount;
-
       const gasPrice = await this.realm.web3.eth.getGasPrice();
       const gas = await resolveCallback(requestId).estimateGas({ from });
-
       const transaction = await resolveCallback(requestId).send({
         from,
         gasPrice,
