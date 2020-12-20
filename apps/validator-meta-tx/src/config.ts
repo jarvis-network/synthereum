2b74746991aeb692f6c58f422f8e4aadfc2b1636
asim_jarvis <asim@jarvis.network>
2020-12-20 14:12:26 +0500

    feat(validator-meta-tx): :sparkles: Inherit config from the validator-lib

--- apps/validator-meta-tx/src/config.ts
+++ apps/validator-meta-tx/src/config.ts
@@ -1,8 +1,11 @@
+import {
+  env as validatorLibenv,
+  ENV as validatorLibENV,
+} from '@jarvis-network/validator-lib';
 import { assertIsString } from '@jarvis-network/web3-utils/base/asserts';
-
 const { PORT, HOST, NODE_ENV } = process.env;
 
-export interface ENV {
+export interface ENV extends validatorLibENV {
   PORT: number;
   HOST: string;
   NODE_ENV: string;
@@ -11,4 +14,5 @@ export const env: ENV = {
   PORT: parseInt(PORT as string),
   HOST: assertIsString(HOST),
   NODE_ENV: assertIsString(NODE_ENV),
+  ...validatorLibenv,
 };
