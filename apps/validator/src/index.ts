ab8c81c2d09c70ace9c42a3c031d22fc13ea043c
asim_jarvis <asim@jarvis.network>
2020-12-20 14:07:27 +0500

    feat(validator-lib): :recycle: Move the realm from validator to lib

--- apps/validator/src/index.ts
+++ apps/validator/src/index.ts
@@ -1,19 +1,13 @@
 #!/usr/bin/env node
 
 require('dotenv').config();
-import { getInfuraWeb3 } from '@jarvis-network/web3-utils/apis/infura';
-import { setPrivateKey_DevelopmentOnly } from '@jarvis-network/web3-utils/eth/web3-instance';
-import { loadRealm } from '@jarvis-network/synthereum-contracts/dist/src/core/load-realm';
+import { getSynthereumRealmWithInfuraWeb3 } from '@jarvis-network/validator-lib';
 import { env } from './config';
 import SynFiatKeeper from './services/SynFiatKeeper';
-import { SynthereumRealmWithWeb3 } from '@jarvis-network/synthereum-contracts/dist/src/core/types';
-import { SupportedNetworkName } from '@jarvis-network/synthereum-contracts/dist/src/config';
-
 process.on('unhandledRejection', (reason, p) => {
   console.error('Unhandled promise rejection: ', p, 'reason:', reason);
   process.exit(13);
 });
-
 main()
   .then(() => {
     console.log('Synthereum Validator exiting');
@@ -22,15 +16,6 @@ main()
     console.error(err);
     process.exit(1);
   });
-
-async function getSynthereumRealmWithInfuraWeb3(): Promise<
-  SynthereumRealmWithWeb3<SupportedNetworkName>
-> {
-  const netId = env.NETWORK_ID;
-  const web3 = getInfuraWeb3(netId);
-  setPrivateKey_DevelopmentOnly(web3, env.PRIVATE_KEY);
-  return await loadRealm(web3, netId);
-}
 
 async function main() {
   console.log('Synthereum Validator starting');
--- libs/validator-lib/src/index.ts
+++ libs/validator-lib/src/index.ts
@@ -1,4 +1,5 @@
 export * from './config';
 export * from './interfaces';
 export * from './log';
+export * from './realm';
 export * from './requests';
