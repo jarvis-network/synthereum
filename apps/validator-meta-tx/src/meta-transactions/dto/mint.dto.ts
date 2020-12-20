aa282ddf596ddeb9f35295a015e3dbc73912ea59
asim_jarvis <asim@jarvis.network>
2020-12-20 17:08:30 +0500

    feat(validator-meta-tx): :art: Use the yup validator and error messages

--- apps/validator-meta-tx/src/meta-transactions/dto/mint.dto.ts
+++ apps/validator-meta-tx/src/meta-transactions/dto/mint.dto.ts
@@ -1,14 +1,61 @@
-import { SyntheticSymbol } from '@jarvis-network/synthereum-contracts/dist/src/config/data/all-synthetic-asset-symbols';
-import { IsString } from 'class-validator';
-import { ETHAddressValidator } from '../../shared/decorator/validate.decorator';
+import {
+  assertIsSyntheticSymbol,
+  SyntheticSymbol,
+} from '@jarvis-network/synthereum-contracts/dist/src/config/data/all-synthetic-asset-symbols';
+import { assertIsAddress } from '@jarvis-network/web3-utils/eth/address';
+import * as yup from 'yup';
+import { ErrorGenerator } from '../../erros/generator';
 
-export class MintRequestDTO {
-  @ETHAddressValidator()
-  sender!: string;
-  @IsString()
-  asset!: SyntheticSymbol;
-  @IsString()
-  collateral_amount!: string;
-  @IsString()
-  num_tokens!: string;
-}
+export type MintRequestParams = {
+  sender: string;
+  asset: SyntheticSymbol;
+  collateral_amount: string;
+  num_tokens: string;
+};
+
+export const mintRequestSchema = yup
+  .object({
+    sender: yup
+      .string()
+      .trim()
+      .required(ErrorGenerator.Required<MintRequestParams>('sender')),
+    collateral_amount: yup
+      .string()
+      .trim()
+      .required(
+        ErrorGenerator.Required<MintRequestParams>('collateral_amount'),
+      ),
+    num_tokens: yup
+      .string()
+      .trim()
+      .required(ErrorGenerator.Required<MintRequestParams>('num_tokens')),
+    asset: yup
+      .mixed<SyntheticSymbol>()
+      .required(ErrorGenerator.Required<MintRequestParams>('asset')),
+  })
+  .test(
+    'sender',
+    ErrorGenerator.NotValiad<MintRequestParams>('sender', 'eth_address'),
+    (value: MintRequestParams) => {
+      try {
+        assertIsAddress(value.sender);
+      } catch (error) {
+        return false;
+      }
+
+      return true;
+    },
+  )
+  .test(
+    'asset',
+    ErrorGenerator.NotValiad<MintRequestParams>('asset', 'type'),
+    (value: MintRequestParams) => {
+      try {
+        assertIsSyntheticSymbol(value.sender);
+      } catch (error) {
+        return false;
+      }
+
+      return true;
+    },
+  );
--- apps/validator-meta-tx/src/meta-transactions/metatx.controller.ts
+++ apps/validator-meta-tx/src/meta-transactions/metatx.controller.ts
@@ -1,8 +1,9 @@
 import { createEverLogger } from '@jarvis-network/validator-lib';
 import { Body, Controller, Post } from '@nestjs/common';
+import { ParseError } from 'src/erros/messages';
 import { ExchangeRequestDTO } from './dto/exchange.dto';
-import { MintRequestDTO } from './dto/mint.dto';
-import { RedeemRequestDTO } from './dto/redeem.dto';
+import { MintRequestParams, mintRequestSchema } from './dto/mint.dto';
+import { RedeemRequestParams, redeemRequestSchema } from './dto/redeem.dto';
 import { MetaTransactionService } from './metatx.service';
 @Controller('/meta-tx')
 export class MetaTransactionController {
@@ -18,17 +19,37 @@ export class MetaTransactionController {
     return message;
   }
   @Post('mint')
-  async mintRequest(@Body() dto: MintRequestDTO) {
-    this.logger.info(`Request Received >> ${JSON.stringify(dto, null, ' ')}`);
-    const message = this.metaTxService.mintRequest(dto);
-    this.logger.info(`Generated payload >> ${message}`);
-    return message;
+  async mintRequest(@Body() dto: MintRequestParams) {
+    try {
+      await mintRequestSchema.validate(dto, {
+        abortEarly: false,
+      });
+      this.logger.info(`Request Received >> ${JSON.stringify(dto, null, ' ')}`);
+
+      const message = { check: true };
+      // this.metaTxService.mintRequest(dto);
+      //this.logger.info(`Generated payload >> ${message}`);
+      return message;
+    } catch (error) {
+      this.logger.error(error);
+      return ParseError(error, 'Validation Failed');
+    }
   }
   @Post('redeem')
-  async redeemRequest(@Body() dto: RedeemRequestDTO) {
-    this.logger.info(`Request Received >> ${JSON.stringify(dto, null, ' ')}`);
-    const message = this.metaTxService.redeemRequest(dto);
-    this.logger.info(`Generated payload >> ${message}`);
-    return message;
+  async redeemRequest(@Body() dto: RedeemRequestParams) {
+    try {
+      await redeemRequestSchema.validate(dto, {
+        abortEarly: false,
+      });
+      this.logger.info(`Request Received >> ${JSON.stringify(dto, null, ' ')}`);
+
+      this.logger.info(`Request Received >> ${JSON.stringify(dto, null, ' ')}`);
+      const message = this.metaTxService.redeemRequest(dto);
+      this.logger.info(`Generated payload >> ${message}`);
+      return message;
+    } catch (error) {
+      this.logger.error(error);
+      return ParseError(error, 'Validation Failed');
+    }
   }
 }
