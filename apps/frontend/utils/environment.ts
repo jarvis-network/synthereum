c9e2d993a8f24b111004aee7598030a4ea0c0a06
Petar Kirov <petar.p.kirov@gmail.com>
2020-12-23 07:24:34 +0200

    fix(frontend): :wrench: Change default price feed endpoint url to pricefeed-dev.jarvis.exchange

--- apps/frontend/utils/environment.ts
+++ apps/frontend/utils/environment.ts
@@ -1,5 +1,6 @@
 export const getPriceFeedEndpoint = () =>
-  process.env.NEXT_PUBLIC_PRICE_FEED_ROOT || 'ws://localhost:7770';
+  process.env.NEXT_PUBLIC_PRICE_FEED_ROOT ||
+  'ws://pricefeed-dev.jarvis.exchange:8080';
 
 export const getFrontendSupportedAssets = () => {
   return process.env.NEXT_PUBLIC_SUPPORTED_ASSETS
