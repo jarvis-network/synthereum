5cfdb407f5d469b9043a071b700203e523affffb
dzek <git-public@dzek.eu>
2020-12-29 12:26:54 +0100

    [frontend]: filtered FPN warnings from console

--- apps/frontend/pages/_app.tsx
+++ apps/frontend/pages/_app.tsx
@@ -4,6 +4,8 @@ import { Provider as StateProvider } from 'react-redux';
 
 import { styled } from '@jarvis-network/ui';
 
+import "@/utils/consoleErrorFilter";
+
 import { useStore } from '@/state/store';
 import { AppThemeProvider } from '@/components/AppThemeProvider';
 import { AuthProvider } from '@/components/auth/AuthProvider';
