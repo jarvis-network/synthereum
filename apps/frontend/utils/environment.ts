1d2576db8e75c98e8322a018c195a0965c69bea8
Petar Kirov <petar.p.kirov@gmail.com>
2020-12-30 19:24:46 +0200

    improvement(frontend): :wrench: Use the official price-feed endpoint now that it is working

--- apps/frontend/utils/environment.ts
+++ apps/frontend/utils/environment.ts
@@ -1,6 +1,5 @@
 export const getPriceFeedEndpoint = () =>
-  process.env.NEXT_PUBLIC_PRICE_FEED_ROOT ||
-  'ws://pricefeed-dev.jarvis.exchange:8080';
+  process.env.NEXT_PUBLIC_PRICE_FEED_ROOT || 'wss://pricefeed.jarvis.exchange';
 
 export const getFrontendSupportedAssets = () => {
   return process.env.NEXT_PUBLIC_SUPPORTED_ASSETS
