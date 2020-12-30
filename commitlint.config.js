84b35011606a33c5a45fcf5e2d7ffef628cc5e95
asim_jarvis <asim@jarvis.network>
2020-12-30 14:35:12 +0500

    [ci]: make commitlint optional

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,13 @@
+const ERROR = 2;
+const WARNING = 1;
+
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-max-line-length': [ERROR, 'always', 80],
+    'footer-max-line-length': [ERROR, 'always', 80],
+    'header-max-length': [ERROR, 'always', 130],
+    'subject-empty': [WARNING, 'never'],
+    'type-empty': [WARNING, 'never'],
+  },
 };
