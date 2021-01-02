19fd27719b88b16aa1fc20c45592a820368b46b4
Petar Kirov <petar.p.kirov@gmail.com>
2021-01-02 12:55:43 +0200

    fix(root): :hammer: Restore the ability to manually write a commit message

--- husky.sh
+++ husky.sh
@@ -5,5 +5,5 @@ set -euo pipefail
 if git branch | grep -q 'no branch'; then
     git --no-pager log -1 --format=oneline
 else
-    exec </dev/tty && git cz --hook
+    exec </dev/tty && git cz --hook || true
 fi
