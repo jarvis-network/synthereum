ed18d16aa6bfb7080965048ac12033c7710d3566
Alessandro Bergamaschi <alessandro@jarvis.network>
2021-01-05 19:46:12 +0100

    feat(contracts): add imports of uma contracts needed for local migration of uma infrastructure

---
+++ libs/contracts/contracts/test/UmaInfrastructure.sol
@@ -0,0 +1,39 @@
+import {
+  Finder
+} from '@jarvis-network/uma-core/contracts/oracle/implementation/Finder.sol';
+import {
+  Timer
+} from '@jarvis-network/uma-core/contracts/common/implementation/Timer.sol';
+import {
+  VotingToken
+} from '@jarvis-network/uma-core/contracts/oracle/implementation/VotingToken.sol';
+import {
+  TokenMigrator
+} from '@jarvis-network/uma-core/contracts/oracle/implementation/TokenMigrator.sol';
+import {
+  Voting
+} from '@jarvis-network/uma-core/contracts/oracle/implementation/Voting.sol';
+import {
+  IdentifierWhitelist
+} from '@jarvis-network/uma-core/contracts/oracle/implementation/IdentifierWhitelist.sol';
+import {
+  Registry
+} from '@jarvis-network/uma-core/contracts/oracle/implementation/Registry.sol';
+import {
+  FinancialContractsAdmin
+} from '@jarvis-network/uma-core/contracts/oracle/implementation/FinancialContractsAdmin.sol';
+import {
+  Store
+} from '@jarvis-network/uma-core/contracts/oracle/implementation/Store.sol';
+import {
+  Governor
+} from '@jarvis-network/uma-core/contracts/oracle/implementation/Governor.sol';
+import {
+  DesignatedVotingFactory
+} from '@jarvis-network/uma-core/contracts/oracle/implementation/DesignatedVotingFactory.sol';
+import {
+  TestnetERC20
+} from '@jarvis-network/uma-core/contracts/common/implementation/TestnetERC20.sol';
+import {
+  OptimisticOracle
+} from '@jarvis-network/uma-core/contracts/oracle/implementation/OptimisticOracle.sol';
