66d414c32d580e03ee80c6529bf0ffd7ad35e97b
Alessandro Bergamaschi <alessandro@jarvis.network>
2021-01-07 15:07:10 +0100

    feat(contracts): Add integration for hardhat on public networks

--- libs/contracts/test/truffle-fixture.js
+++ libs/contracts/test/truffle-fixture.js
@@ -1,4 +1,5 @@
 require('dotenv').config({ path: './.env.migration' });
+const { parseBoolean } = require('@jarvis-network/web3-utils/base/asserts');
 //Synthereum contracts to deploy Synthereum infrastructure for local hardhat test
 const deployFinder = require('../migrations/2_deploy_finder.js');
 const deployDeployer = require('../migrations/3_deploy_deployer.js');
@@ -6,6 +7,11 @@ const deployPoolRegistry = require('../migrations/4_deploy_pool_registry.js');
 const deployFactoryVersioning = require('../migrations/5_deploy_factory_versioning.js');
 const deployDerivativeVersions = require('../migrations/6_deploy_derivative_versions.js');
 const deployPoolVersions = require('../migrations/7_deploy_pool_versions.js');
+
+const deployPoolAndDerivative = require('../migrations/8_add_pool_and_derivative.js');
+const deployOnlyPool = require('../migrations/9_add_only_pool.js');
+const deployOnlyDerivative = require('../migrations/10_add_only_derivative.js');
+
 //Uma contracts to deploy Uma infrastructure for local hardhat test
 const umaDeployFinder = require('@jarvis-network/uma-core/migrations/2_deploy_finder.js');
 const umaDeployTimer = require('@jarvis-network/uma-core/migrations/3_deploy_timer.js');
@@ -21,7 +27,8 @@ const UmaDeployOptimisticOracle = require('@jarvis-network/uma-core/migrations/1
 module.exports = async ({ network, web3 }) => {
   const accounts = await web3.eth.getAccounts();
   const networkId = await web3.eth.net.getId();
-  const newUmaDeployment = process.env.NEW_UMA_INFRASTRUCTURE ?? false;
+  const newUmaDeployment =
+    parseBoolean(process.env.NEW_UMA_INFRASTRUCTURE) ?? false;
   if (
     (networkId != 1 && networkId != 3 && networkId != 4 && networkId != 42) ||
     newUmaDeployment
@@ -37,10 +44,34 @@ module.exports = async ({ network, web3 }) => {
     await UmaDeployDesignatedVotingFactory(null, network.name, accounts);
     await UmaDeployOptimisticOracle(null, network.name, accounts);
   }
-  await deployFinder(null, network.name, accounts);
-  await deployDeployer(null, network.name, accounts);
-  await deployPoolRegistry(null, network.name, accounts);
-  await deployFactoryVersioning(null, network.name, accounts);
-  await deployDerivativeVersions(null, network.name, accounts);
-  await deployPoolVersions(null, network.name, accounts);
+  if (process.env.MIGRATION_TYPE == 'infrastructure') {
+    console.log('Deploying infrastracture...');
+    await deployFinder(null, network.name, accounts);
+    await deployDeployer(null, network.name, accounts);
+    await deployPoolRegistry(null, network.name, accounts);
+    await deployFactoryVersioning(null, network.name, accounts);
+  } else if (process.env.MIGRATION_TYPE == 'derivativeVersions') {
+    console.log('Deploying derivative versions...');
+    await deployDerivativeVersions(null, network.name, accounts);
+  } else if (process.env.MIGRATION_TYPE == 'poolVersions') {
+    console.log('Deploying pool versions...');
+    await deployPoolVersions(null, network.name, accounts);
+  } else if (process.env.MIGRATION_TYPE == 'pool-and-derivative') {
+    console.log('Deploying pool and derivatives...');
+    await deployPoolAndDerivative(null, network.name, accounts);
+  } else if (process.env.MIGRATION_TYPE == 'only-pool') {
+    console.log('Deploying only pool...');
+    await deployOnlyPool(null, network.name, accounts);
+  } else if (process.env.MIGRATION_TYPE == 'only-derivative') {
+    console.log('Deploying only derivatives...');
+    await deployOnlyDerivative(null, network.name, accounts);
+  } else {
+    console.log('Deploying all...');
+    await deployFinder(null, network.name, accounts);
+    await deployDeployer(null, network.name, accounts);
+    await deployPoolRegistry(null, network.name, accounts);
+    await deployFactoryVersioning(null, network.name, accounts);
+    await deployDerivativeVersions(null, network.name, accounts);
+    await deployPoolVersions(null, network.name, accounts);
+  }
 };
