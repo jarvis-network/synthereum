afad1594a990d9c5d3b8efabef6bb60e686aeca8
Yurii Tokarskyi <ytokarskyi@lemonunit.com>
2021-01-20 15:36:06 +0200

    [frontend/PWA] Generate SW during the build and make website installable

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,5 +1,14 @@
+const withPWA = require('next-pwa');
+
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = withBundleAnalyzer({});
+module.exports = withBundleAnalyzer(withPWA({
+  pwa: {
+    disable: false,
+    register: true,
+    dest: "public",
+    scope: "/"
+  }
+}));
