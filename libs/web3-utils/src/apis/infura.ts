458ffc461e389b2169c3a8fa206a4e370a7d3b71
Petar Kirov <petar.p.kirov@gmail.com>
2021-01-21 17:20:43 +0200

    fix(web3-utils/apis/infura/getInfuraWeb3): Pass `protocol` param to `getInfuraEndpoint`

--- libs/web3-utils/src/apis/infura.ts
+++ libs/web3-utils/src/apis/infura.ts
@@ -1,37 +1,28 @@
 import Web3 from 'web3';
-import {
-  Network,
-  ToNetworkName,
-  toNetworkName,
-  ValueOnNetwork,
-} from '../eth/networks';
+import { Network, toNetworkName, ValueOnNetwork } from '../eth/networks';
 import { env } from '../config';
 import type { Web3On } from '../eth/web3-instance';
 
-enum Protocol {
-  wss = 'wss:',
-  https = 'https:',
-}
+export type Protocol = 'wss' | 'https';
 
 export function getInfuraEndpoint<Net extends Network>(
   network: Net,
-  protocol: Protocol = Protocol.https,
+  protocol: Protocol = 'https',
 ): ValueOnNetwork<string, Net> {
   const networkName = toNetworkName(network);
   const projectId = env.infuraProjectId;
-  return `${protocol}//${networkName}.infura.io/v3/${projectId}` as ValueOnNetwork<
-    string,
-    Net
-  >;
+  return (`${protocol}://${networkName}.infura.io` +
+    (protocol === 'wss' ? '/ws' : '') +
+    `/v3/${projectId}`) as ValueOnNetwork<string, Net>;
 }
 
 export function getInfuraWeb3<Net extends Network>(
   network: Net,
-  protocol: Protocol = Protocol.https,
+  protocol: Protocol = 'https',
 ): Web3On<Net> {
-  const url = getInfuraEndpoint(network);
+  const url = getInfuraEndpoint(network, protocol);
   const result =
-    protocol === Protocol.https
+    protocol === 'https'
       ? new Web3(new Web3.providers.HttpProvider(url))
       : new Web3(new Web3.providers.WebsocketProvider(url));
   return result as Web3On<Net>;
