0b4999b9b6f00721f0eaafbe2af4a91141d7ca97
Alessandro Bergamaschi <alessandro@jarvis.network>
2021-02-02 15:14:10 +0100

    feat(contracts): Add deployment of SynthereumPoolOnChainPriceFeedFactory

--- libs/contracts/migrations/7_deploy_pool_versions.js
+++ libs/contracts/migrations/7_deploy_pool_versions.js
@@ -4,11 +4,18 @@ const { getDeploymentInstance } = require('../utils/deployment.js');
 var SynthereumFinder = artifacts.require('SynthereumFinder');
 var SynthereumTICHelper = artifacts.require('SynthereumTICHelper');
 var SynthereumPoolLib = artifacts.require('SynthereumPoolLib');
+var SynthereumPoolOnChainPriceFeedLib = artifacts.require(
+  'SynthereumPoolOnChainPriceFeedLib',
+);
 var SynthereumFactoryVersioning = artifacts.require(
   'SynthereumFactoryVersioning',
 );
 var SynthereumTICFactory = artifacts.require('SynthereumTICFactory');
 var SynthereumPoolFactory = artifacts.require('SynthereumPoolFactory');
+var SynthereumPoolOnChainPriceFeedFactory = artifacts.require(
+  'SynthereumPoolOnChainPriceFeedFactory',
+);
+
 var poolVersions = require('../data/pool-versions.json');
 const { getKeysForNetwork, deploy } = require('@jarvis-network/uma-common');
 
@@ -128,5 +135,59 @@ module.exports = async function (deployer, network, accounts) {
           )
           .send({ from: maintainer });
     console.log('PoolFactory added to SynthereumFactoryVersioning');
+  }
+  if (poolVersions[networkId]?.PoolOnChainPriceFeedFactory?.isEnabled ?? true) {
+    if (SynthereumPoolOnChainPriceFeedLib.setAsDeployed) {
+      const {
+        contract: synthereumPoolOnChainPriceFeedLibInstance,
+      } = await deploy(deployer, network, SynthereumPoolOnChainPriceFeedLib, {
+        from: keys.deployer,
+      });
+
+      // Due to how truffle-plugin works, it statefully links it
+      // and throws an error if its already linked. So we'll just ignore it...
+      try {
+        await SynthereumPoolOnChainPriceFeedFactory.link(
+          synthereumPoolOnChainPriceFeedLibInstance,
+        );
+      } catch (e) {
+        // Allow this to fail in the Buidler case.
+      }
+    } else {
+      // Truffle
+      await deploy(deployer, network, SynthereumPoolOnChainPriceFeedLib, {
+        from: keys.deployer,
+      });
+      await deployer.link(
+        SynthereumPoolOnChainPriceFeedLib,
+        SynthereumPoolOnChainPriceFeedFactory,
+      );
+    }
+    await deploy(
+      deployer,
+      network,
+      SynthereumPoolOnChainPriceFeedFactory,
+      isDeployedFinder
+        ? synthereumFinderInstance.address
+        : synthereumFinderInstance.options.address,
+      { from: keys.deployer },
+    );
+    const synthereumPoolOnChainPriceFeedFactory = await SynthereumPoolOnChainPriceFeedFactory.deployed();
+
+    isDeployedFactoryVersioning
+      ? await synthereumFactoryVersioningInstance.setPoolFactory(
+          poolVersions[networkId]?.PoolOnChainPriceFeedFactory?.version ?? 3,
+          synthereumPoolOnChainPriceFeedFactory.address,
+          { from: maintainer },
+        )
+      : await synthereumFactoryVersioningInstance.methods
+          .setPoolFactory(
+            poolVersions[networkId]?.PoolOnChainPriceFeedFactory?.version ?? 3,
+            synthereumPoolOnChainPriceFeedFactory.address,
+          )
+          .send({ from: maintainer });
+    console.log(
+      'PoolOnChainPriceFeedFactory added to SynthereumFactoryVersioning',
+    );
   }
 };
