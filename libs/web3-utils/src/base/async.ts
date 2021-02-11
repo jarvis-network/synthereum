e452be54be3b5ac9cd24a03e76a3d72c59b31605
Petar Kirov <petar.p.kirov@gmail.com>
2021-02-11 23:43:23 +0200

    improve(web3-utils/base/async): Add `executeInSequence` function

--- libs/web3-utils/src/base/async.ts
+++ libs/web3-utils/src/base/async.ts
@@ -7,3 +7,15 @@
 export function delay(miliseconds: number): Promise<void> {
   return new Promise(resolve => setTimeout(resolve, miliseconds));
 }
+
+type MapToPromiseFunctions<T> = { [P in keyof T]: () => Promise<T[P]> };
+
+export async function executeInSequence<Args extends any[]>(
+  ...promises: MapToPromiseFunctions<Args>
+): Promise<Args> {
+  const result: Args = ([] as unknown) as Args;
+  for (const p of promises) {
+    result.push(await p());
+  }
+  return result;
+}
