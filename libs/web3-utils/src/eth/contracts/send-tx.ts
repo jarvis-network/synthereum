399e7854469e2ff99871a30fc16bdc2a03414bd1
Petar Kirov <petar.p.kirov@gmail.com>
2021-02-11 04:06:45 +0200

    refactor(web3-utils/src/eth/contracts/send-tx): Move code from `web3-utils/eth/web3` here

---
+++ libs/web3-utils/src/eth/contracts/send-tx.ts
@@ -0,0 +1,89 @@
+import type { NetworkName } from '../networks';
+import type { PromiEvent, TransactionReceipt } from 'web3-core';
+import { EventEmitter } from 'events';
+import { Web3On } from '../web3-instance';
+
+export function once<T>(
+  promiEvent: PromiEvent<T>,
+  type: 'sending',
+): Promise<void>;
+export function once<T>(promiEvent: PromiEvent<T>, type: 'sent'): Promise<void>;
+export function once<T>(
+  promiEvent: PromiEvent<T>,
+  type: 'transactionHash',
+): Promise<string>;
+export function once<T>(
+  promiEvent: PromiEvent<T>,
+  type: 'receipt',
+): Promise<TransactionReceipt>;
+export function once<T>(
+  promiEvent: PromiEvent<T>,
+  type: 'confirmation',
+  maxConfirmations?: number,
+): Promise<[number, TransactionReceipt, string]>;
+export function once<T>(
+  promiEvent: PromiEvent<T>,
+  type: Parameters<typeof promiEvent.once>[0] | 'sending' | 'sent',
+  maxConfirmations = 1,
+) {
+  return new Promise((resolve, reject) => {
+    promiEvent.once('error', reject);
+    switch (type) {
+      case 'sending':
+        promiEvent.once(type, resolve);
+        break;
+      case 'sent':
+        promiEvent.once(type, resolve);
+        break;
+      case 'transactionHash':
+      case 'receipt':
+        promiEvent.once(type, resolve);
+        break;
+      case 'confirmation':
+        function onConfirm(
+          confirmations: number,
+          receipt: TransactionReceipt,
+          blockHash?: string,
+        ) {
+          if (confirmations == maxConfirmations) {
+            ((promiEvent as unknown) as EventEmitter).off(
+              'confirmation',
+              onConfirm,
+            );
+            resolve([confirmations, receipt, blockHash]);
+          }
+        }
+
+        promiEvent.on(type, onConfirm);
+        break;
+    }
+  });
+}
+
+export async function logTransactionStatus<T, Net extends NetworkName>(
+  web3: Web3On<Net>,
+  promiEvent: PromiEvent<T>,
+) {
+  await once(promiEvent, 'sending');
+  console.log('[1/4] Sending tx...');
+
+  await once(promiEvent, 'sent');
+  console.log('[2/4] Tx sent. Waiting for hash...');
+
+  const txHash = await once(promiEvent, 'transactionHash');
+  console.log(
+    `[3/4]: Tx hash: '${txHash}'. Waiting for ${web3.eth.transactionConfirmationBlocks} confirmations...`,
+  );
+
+  const [confirmation, receipt] = await once(
+    promiEvent,
+    'confirmation',
+    web3.eth.transactionConfirmationBlocks,
+  );
+  const { gasUsed, blockNumber } = receipt;
+  console.log(
+    `[4/4]: Tx confirmed ${confirmation} time(s). Gas used: ${gasUsed} | block number: ${blockNumber}`,
+  );
+
+  return receipt;
+}
--- libs/web3-utils/src/eth/web3.ts
+++ libs/web3-utils/src/eth/web3.ts
@@ -1,14 +1,6 @@
-import type { EventEmitter } from 'events';
-import BN from 'bn.js';
-import type {
-  BlockNumber,
-  PromiEvent,
-  Transaction,
-  TransactionReceipt,
-} from 'web3-core';
+import type { BlockNumber, Transaction } from 'web3-core';
 import { getContractTxs } from '../apis/etherscan';
 import { parseInteger } from '../base/asserts';
-import type { NonPayableTransactionObject } from './contracts/typechain/types';
 import { NetworkName, Web3On } from './web3-instance';
 import { asyncLowerBound } from '../base/sorting';
 
@@ -50,89 +42,4 @@ export async function getContractTransactions<Net extends NetworkName>(
       async tx => await web3.eth.getTransaction(tx.hash),
     ),
   );
-}
-
-export function once<T>(
-  promiEvent: PromiEvent<T>,
-  type: 'sending',
-): Promise<void>;
-export function once<T>(promiEvent: PromiEvent<T>, type: 'sent'): Promise<void>;
-export function once<T>(
-  promiEvent: PromiEvent<T>,
-  type: 'transactionHash',
-): Promise<string>;
-export function once<T>(
-  promiEvent: PromiEvent<T>,
-  type: 'receipt',
-): Promise<TransactionReceipt>;
-export function once<T>(
-  promiEvent: PromiEvent<T>,
-  type: 'confirmation',
-  maxConfirmations?: number,
-): Promise<[number, TransactionReceipt, string]>;
-export function once<T>(
-  promiEvent: PromiEvent<T>,
-  type: Parameters<typeof promiEvent.once>[0] | 'sending' | 'sent',
-  maxConfirmations = 1,
-) {
-  return new Promise((resolve, reject) => {
-    promiEvent.once('error', reject);
-    switch (type) {
-      case 'sending':
-        promiEvent.once(type, resolve);
-        break;
-      case 'sent':
-        promiEvent.once(type, resolve);
-        break;
-      case 'transactionHash':
-      case 'receipt':
-        promiEvent.once(type, resolve);
-        break;
-      case 'confirmation':
-        function onConfirm(
-          confirmations: number,
-          receipt: TransactionReceipt,
-          blockHash?: string,
-        ) {
-          if (confirmations == maxConfirmations) {
-            ((promiEvent as unknown) as EventEmitter).off(
-              'confirmation',
-              onConfirm,
-            );
-            resolve([confirmations, receipt, blockHash]);
-          }
-        }
-
-        promiEvent.on(type, onConfirm);
-        break;
-    }
-  });
-}
-
-export async function logTransactionStatus<T, Net extends NetworkName>(
-  web3: Web3On<Net>,
-  promiEvent: PromiEvent<T>,
-) {
-  await once(promiEvent, 'sending');
-  console.log('[1/4] Sending tx...');
-
-  await once(promiEvent, 'sent');
-  console.log('[2/4] Tx sent. Waiting for hash...');
-
-  const txHash = await once(promiEvent, 'transactionHash');
-  console.log(
-    `[3/4]: Tx hash: '${txHash}'. Waiting for ${web3.eth.transactionConfirmationBlocks} confirmations...`,
-  );
-
-  const [confirmation, receipt] = await once(
-    promiEvent,
-    'confirmation',
-    web3.eth.transactionConfirmationBlocks,
-  );
-  const { gasUsed, blockNumber } = receipt;
-  console.log(
-    `[4/4]: Tx confirmed ${confirmation} time(s). Gas used: ${gasUsed} | block number: ${blockNumber}`,
-  );
-
-  return receipt;
 }
