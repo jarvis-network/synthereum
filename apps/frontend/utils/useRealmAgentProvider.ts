054f1fef8ea7ac67a43f70416e1511a6a2dc5d0f
Dimitar Nestorov <dimitar.nestorov@jarvis.network>
2021-03-05 15:57:29 +0200

    chore(contracts): :recycle: Realm only loads the pools for a single version now

--- apps/frontend/utils/useRealmAgentProvider.ts
+++ apps/frontend/utils/useRealmAgentProvider.ts
@@ -1,4 +1,3 @@
-import { useReduxSelector } from '@/state/useReduxSelector';
 import { useEffect, useRef } from 'react';
 import { Web3On } from '@jarvis-network/web3-utils/eth/web3-instance';
 import { AddressOn } from '@jarvis-network/web3-utils/eth/address';
@@ -6,16 +5,26 @@ import { SupportedNetworkName } from '@jarvis-network/synthereum-contracts/dist/
 import { loadRealm } from '@jarvis-network/synthereum-contracts/dist/src/core/load-realm';
 import { RealmAgent } from '@jarvis-network/synthereum-contracts/dist/src/core/realm-agent';
 import { NETWORK_ID } from './environment';
-import { PoolVersion } from '@jarvis-network/synthereum-contracts/dist/src/core/types/pools';
+import {
+  PoolsForVersion,
+  PoolVersion,
+} from '@jarvis-network/synthereum-contracts/dist/src/core/types/pools';
 import type { BehaviorSubject } from 'rxjs';
 import type Web3 from 'web3';
 import { useStore } from '@/state/store';
+
+const poolVersion = (process.env.NEXT_PUBLIC_POOL_VERSION ||
+  'v1') as PoolVersion;
 
 export function useRealmAgentProvider(
   store: ReturnType<typeof useStore>,
   web3$: BehaviorSubject<Web3 | null>,
   realmAgent$: BehaviorSubject<RealmAgent | null>,
 ) {
+  const poolsRef = useRef<PoolsForVersion<
+    typeof poolVersion,
+    SupportedNetworkName
+  > | null>(null);
   const web3Ref = useRef(web3$.value);
   const addressRef = useRef(store.getState().auth?.address);
   function provide() {
@@ -27,12 +36,15 @@ export function useRealmAgentProvider(
       return;
     }
 
-    loadRealm(web3 as Web3On<SupportedNetworkName>, NETWORK_ID).then(realm => {
+    loadRealm(web3 as Web3On<SupportedNetworkName>, NETWORK_ID, {
+      [poolVersion]: poolsRef.current,
+    }).then(realm => {
+      poolsRef.current = realm.pools[poolVersion]!;
       realmAgent$.next(
         new RealmAgent(
           realm,
           address as AddressOn<typeof NETWORK_ID>,
-          (process.env.NEXT_PUBLIC_POOL_VERSION || 'v1') as PoolVersion,
+          poolVersion,
         ),
       );
     });
--- libs/contracts/src/core/load-realm.ts
+++ libs/contracts/src/core/load-realm.ts
@@ -33,6 +33,10 @@ import type {
 } from './types/pools';
 import type { SynthereumRealmWithWeb3 } from './types/realm';
 
+type PoolVersionsToLoad<Net extends SupportedNetworkName> = {
+  [Version in PoolVersion]?: PoolsForVersion<Version, Net> | null;
+};
+
 /**
  * Load the default Synthereum Realm.
  * @param web3 Web3 instance to connect to
@@ -41,10 +45,11 @@ import type { SynthereumRealmWithWeb3 } from './types/realm';
 export async function loadRealm<Net extends SupportedNetworkName>(
   web3: Web3On<Net>,
   netId: ToNetworkId<Net>,
+  versionsToLoad?: PoolVersionsToLoad<Net>,
 ): Promise<SynthereumRealmWithWeb3<Net>> {
   const config = synthereumConfig[netId as SupportedNetworkId]
     .contractsDependencies.synthereum as SynthereumContractDependencies<Net>;
-  return loadCustomRealm(web3, netId, config);
+  return loadCustomRealm(web3, netId, config, versionsToLoad);
 }
 
 /**
@@ -57,6 +62,7 @@ export async function loadCustomRealm<Net extends SupportedNetworkName>(
   web3: Web3On<Net>,
   netId: ToNetworkId<Net>,
   config: SynthereumContractDependencies<Net>,
+  versionsToLoad: PoolVersionsToLoad<Net> = { v1: null, v2: null, v3: null },
 ): Promise<SynthereumRealmWithWeb3<Net>> {
   let poolRegistry = getContract(
     web3,
@@ -66,7 +72,9 @@ export async function loadCustomRealm<Net extends SupportedNetworkName>(
 
   const collateralAddress = config.primaryCollateralToken.address;
 
-  const loadAllPools = async <Version extends PoolVersion>(version: Version) => {
+  const loadAllPools = async <Version extends PoolVersion>(
+    version: Version,
+  ) => {
     const pairs = await Promise.all(
       allSyntheticSymbols.map(async symbol => {
         const info = await loadPoolInfo(
@@ -84,15 +92,23 @@ export async function loadCustomRealm<Net extends SupportedNetworkName>(
     return Object.fromEntries(pairs.filter(x => !!x[1]));
   };
   const collateralToken = await getTokenInfo(web3, collateralAddress);
+
+  const pools: SynthereumRealmWithWeb3<Net>['pools'] = {};
+  for (const i in versionsToLoad) {
+    const version = i as PoolVersion;
+    const poolsForVersion = versionsToLoad[version];
+    if (typeof poolsForVersion === 'object' && poolsForVersion !== null) {
+      continue;
+    }
+
+    pools[version] = await loadAllPools(version);
+  }
+
   return {
     web3,
     netId,
     poolRegistry,
-    pools: {
-      v1: await loadAllPools('v1'),
-      v2: await loadAllPools('v2'),
-      v3: await loadAllPools('v3'),
-    },
+    pools,
     // Assume the same collateral token for all synthetics:
     collateralToken,
   };
