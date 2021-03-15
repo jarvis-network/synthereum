f40b115db0c98152c27e891625c7f6ca8b9cc478
Dimitar Nestorov <dimitar.nestorov@jarvis.network>
2021-03-15 14:35:03 +0200

    chore(frontend): Increasing limit to 5000 USDC

--- apps/frontend/utils/environment.ts
+++ apps/frontend/utils/environment.ts
@@ -6,7 +6,7 @@ export const getPriceFeedEndpoint = () =>
   process.env.NEXT_PUBLIC_PRICE_FEED_ROOT || 'wss://pricefeed.jarvis.exchange';
 
 export const MAX_MINT_VALUE = new FPN(
-  process.env.NEXT_PUBLIC_MAX_MINT_VALUE || 500,
+  process.env.NEXT_PUBLIC_MAX_MINT_VALUE || 5000,
 );
 
 assertIsSupportedPoolVersion(process.env.NEXT_PUBLIC_POOL_VERSION || 'v1');
