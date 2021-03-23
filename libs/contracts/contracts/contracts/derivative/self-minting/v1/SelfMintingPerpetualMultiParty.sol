ba8f34c5259ad961fc40cba512adbf2d897165ba
Alessandro Bergamaschi <alessandro@jarvis.network>
2021-03-23 19:55:39 +0100

    feat(contracts): Add SelfMintingPerpetualMultiParty

---
+++ libs/contracts/contracts/contracts/derivative/self-minting/v1/SelfMintingPerpetualMultiParty.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.6.0;
+pragma experimental ABIEncoderV2;
+
+import './SelfMintingPerpetualLiquidatableMultiParty.sol';
+
+contract SelfMintingPerpetualMultiParty is
+  SelfMintingPerpetualLiquidatableMultiParty
+{
+  constructor(ConstructorParams memory params)
+    public
+    SelfMintingPerpetualLiquidatableMultiParty(params)
+  {}
+}
