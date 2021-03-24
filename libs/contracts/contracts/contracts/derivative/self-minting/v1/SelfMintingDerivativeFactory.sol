061cb18eed6fc8307115079ec6e9415ab7850bde
Alessandro Bergamaschi <alessandro@jarvis.network>
2021-03-24 15:13:31 +0100

    feat(contracts): Add SelfMintingDerivativeFactory

---
+++ libs/contracts/contracts/contracts/derivative/self-minting/v1/SelfMintingDerivativeFactory.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.6.12;
+pragma experimental ABIEncoderV2;
+
+import {ISynthereumFinder} from '../../../core/interfaces/IFinder.sol';
+import {
+  IDeploymentSignature
+} from '../../../core/interfaces/IDeploymentSignature.sol';
+import {SynthereumInterfaces} from '../../../core/Constants.sol';
+import {
+  SelfMintingPerpetutalMultiPartyCreator
+} from './SelfMintingPerpetutalMultiPartyCreator.sol';
+
+contract SelfMintingDerivativeFactory is
+  SelfMintingPerpetutalMultiPartyCreator,
+  IDeploymentSignature
+{
+  bytes4 public override deploymentSignature;
+
+  constructor(
+    address _synthereumFinder,
+    address _umaFinder,
+    address _timerAddress
+  )
+    public
+    SelfMintingPerpetutalMultiPartyCreator(
+      _umaFinder,
+      _synthereumFinder,
+      _timerAddress
+    )
+  {
+    deploymentSignature = this.createPerpetual.selector;
+  }
+
+  function createPerpetual(Params memory params)
+    public
+    override
+    returns (address derivative)
+  {
+    address deployer =
+      ISynthereumFinder(synthereumFinder).getImplementationAddress(
+        SynthereumInterfaces.Deployer
+      );
+    require(msg.sender == deployer, 'Sender must be Synthereum deployer');
+    derivative = super.createPerpetual(params);
+  }
+}
