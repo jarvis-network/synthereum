6f442ec02a5deeb92588592bf8817b9a1025a3f7
Petar Kirov <petar.p.kirov@gmail.com>
2021-05-20 09:57:40 +0300

    improve(cli): Enable forwarding of CLI arguments
    For example:
        yarn start cli --args="--script-name is-supported-uma-price-identifier \
            --identifier 'EUR/USD' --network-id=42"

--- apps/cli/scripts/run.sh
+++ apps/cli/scripts/run.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/env bash
-path="./dist/${1:-get-pool-balance}.js"
+
+while true; do
+  case "$1" in
+    # Nx inserts these for some reason - skip them
+    --_=*) shift ;;
+    --scriptName=*) shift ;;
+    --script-name) script_name="$2"; shift 2 ;;
+    --script-name=*) script_name="${1:14}"; shift ;;
+    --) shift; break ;;
+    *) break ;;
+  esac
+done
+
+path="./dist/${script_name:-get-pool-balance}.js"
+
 if [ ! -f "$path" ]; then
     echo "file '$path' doesn't exist - rebuilding"
     yarn build
 fi
-node --unhandled-rejections=strict "$path"
+
+node --unhandled-rejections=strict "$path" "${@}"
