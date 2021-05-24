739163d4004548ef09b7f96b3401506bc0042826
Petar Kirov <petar.p.kirov@gmail.com>
2021-05-24 23:53:08 +0300

    improve(core-utils/config): Call `dotenv` if it is available

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -12,11 +12,14 @@ module.exports = withBundleAnalyzer(
       dest: 'public',
       scope: '/',
     },
-    webpack: config => {
+    webpack: (config, { webpack }) => {
       config.module.rules.push({
         test: /\.md$/,
         use: 'raw-loader',
       });
+      // TODO: Upgrade to Next.js 10 / Webpack 5 to use this PR:
+      // https://github.com/webpack/webpack/pull/11316
+      config.plugins.push(new webpack.IgnorePlugin(/dotenv/));
 
       return config;
     },
--- libs/core-utils/src/config.ts
+++ libs/core-utils/src/config.ts
@@ -1,3 +1,12 @@
+if (typeof process !== 'undefined') {
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    require('dotenv').config();
+  } catch (err) {
+    console.warn('dotenv import failed:', err);
+  }
+}
+
 const { INFURA_PROJECT_ID, DEV_MNEMONIC, ETHERSCAN_API_KEY } = process.env;
 
 export const env = {
