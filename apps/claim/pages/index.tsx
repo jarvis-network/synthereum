6642c7e3edaf674fe6843f5449e0e5f8170b18c4
Dimitar Nestorov <dimitar.nestorov@jarvis.network>
2021-06-07 17:21:29 +0300

    chore(claim): Switch wallet screen if not investor

--- apps/claim/pages/index.tsx
+++ apps/claim/pages/index.tsx
@@ -1,10 +1,10 @@
+import { FPN } from '@jarvis-network/core-utils/dist/base/fixed-point-number';
 import { Tabs, styled, Button } from '@jarvis-network/ui';
 
 import { useReduxSelector } from '@/state/useReduxSelector';
 import { Claim } from '@/components/Claim';
 import { History } from '@/components/History';
-import { useDispatch } from 'react-redux';
-import { setAuthModalVisible } from '@/state/slices/app';
+import { useAuth } from '@jarvis-network/app-toolkit';
 
 const Container = styled.div`
   flex: 1;
@@ -102,15 +102,71 @@ const SignIn = styled.div`
   }
 `;
 
+const SwitchWallet = styled.div`
+  display: flex;
+  padding: 16px;
+  flex-direction: column-reverse !important;
+
+  > button {
+    width: 100%;
+    text-align: center;
+  }
+
+  @media screen and (min-width: ${props =>
+      props.theme.rwd.breakpoints[props.theme.rwd.desktopIndex - 1]}px) {
+    height: calc(275px + 60px);
+    padding: 24px;
+  }
+`;
+
+const Spacer = styled.div`
+  flex-grow: 1;
+`;
+
+const NoAllocatedTokens = styled.div`
+  width: 100%;
+  text-align: center;
+  padding: 16px;
+  background-color: rgba(250, 78, 78, 0.15);
+  color: ${props => props.theme.common.danger};
+  border-radius: 10px;
+  flex-grow: 0 !important;
+  font-size: 18px;
+`;
+
+const Sculpture = styled.img`
+  display: block;
+  margin: 0 auto;
+  margin-bottom: 20px;
+`;
+
 export default function Home(): JSX.Element {
-  const auth = useReduxSelector(state => state.auth);
-  const dispatch = useDispatch();
+  const { auth, claim } = useReduxSelector(state => state);
+  const { logout, login } = useAuth();
 
   return (
     <Container>
       <Content>
         <Title>Claim Your JRT Tokens</Title>
-        {auth ? (
+        {claim && new FPN(claim.investorInfo, true).toNumber() <= 0 ? (
+          <SwitchWallet
+            onClick={() => {
+              logout();
+              setTimeout(login, 100);
+            }}
+          >
+            <Button type="success">Switch Wallet</Button>
+            <Spacer />
+            <NoAllocatedTokens>
+              No allocated tokens for this wallet
+            </NoAllocatedTokens>
+            <Sculpture
+              src="/images/girl-sculpture-dark.png"
+              height="120"
+              alt="No allocated tokens"
+            />
+          </SwitchWallet>
+        ) : auth ? (
           <CustomTabs
             pointer={false}
             tabs={[
@@ -120,10 +176,7 @@ export default function Home(): JSX.Element {
           />
         ) : (
           <SignIn>
-            <Button
-              type="success"
-              onClick={() => dispatch(setAuthModalVisible(true))}
-            >
+            <Button type="success" onClick={() => login()}>
               Sign in
             </Button>
           </SignIn>
