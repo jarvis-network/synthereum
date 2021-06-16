f9106a20b6759c07ba4f951be088c7fcbcfff165
Petar Kirov <petar.p.kirov@gmail.com>
2021-06-16 04:17:15 +0300

    ci: :building_construction: :truck: Re-implement the GitLab CI Pipeline
    * Build and export to registry a minimal `yarn install` Docker image (containing
    installed node_modules) and tag it with the SHA256 of yarn.lock
    
    * Add a job that generates dynamically a yaml CI pipeline config file with
    container image set to the one previously built
    
    * Re-write the stage names
    
    * Move tasks like build, deploy and lint to the child pipeline (as they depend
    on the dynamic image hash)
    
    * Add a new `contracts:test` jobA
    
    * Make the contracts test target in `workspace.json` automatically call `yarn
    contracts build`.
    
    * ci: Remove unused `release` job
    * ci: Adjust the `security scan` job
    
    Co-authored-by: Dimitar Nestorov <dimitar.nestorov@jarvis.network>

---
+++ scripts/generate-pipeline.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+YARN_LOCK_SHA256="$(sha256sum yarn.lock | awk '{ print $1; }')"
+
+cat <<EOF
+workflow:
+  rules:
+    - if: \$CI_MERGE_REQUEST_ID
+
+image: '${REGISTRY}/install:${YARN_LOCK_SHA256}'
+
+variables:
+  GIT_STRATEGY: none
+
+before_script:
+  - cd /src
+  - git init
+  - git remote add jn $CI_PROJECT_URL
+  - git fetch jn $CI_COMMIT_REF_NAME
+  - git switch --discard-changes $CI_COMMIT_REF_NAME
+
+🎨 lint:all:
+  script:
+    - yarn lint:all
+
+🧪 contracts:test:
+  script:
+    - yarn test contracts
+
+🚀 deploy:
+  variables:
+    NETLIFY_AUTH_TOKEN: '\$NETLIFY_AUTH_TOKEN'
+  parallel:
+    matrix:
+      - TARGET: frontend
+        NETLIFY_SITE_ID: '\$NETLIFY_SITE_ID_FRONTEND'
+      - TARGET: borrowing
+        NETLIFY_SITE_ID: '\$NETLIFY_SITE_ID_BORROWING'
+      - TARGET: claim
+        NETLIFY_SITE_ID: '\$NETLIFY_SITE_ID_CLAIM'
+  script:
+    - yarn build \$TARGET
+    - yarn netlify deploy --dir "/src/apps/\$TARGET" --site "\$NETLIFY_SITE_ID" --auth "\$NETLIFY_AUTH_TOKEN"
+EOF
