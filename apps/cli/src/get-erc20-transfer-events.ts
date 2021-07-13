676257e2ace5450dfc4a2c4f50de4ee7ae0489f4
Andrea Di Nenno <dinennoandrea@gmail.com>
2021-07-13 12:25:58 +0200

    refactor(cli): :recycle: Refactor get-erc20-transfer-events to user common code
    Refactor get-erc20-transfer-events to user common code

--- apps/cli/src/get-erc20-transfer-events.ts
+++ apps/cli/src/get-erc20-transfer-events.ts
@@ -1,8 +1,5 @@
-import yargs from 'yargs';
-import { getInfuraWeb3 } from '@jarvis-network/core-utils/dist/apis/infura';
 import { getClosestBlock } from '@jarvis-network/core-utils/dist/eth/web3';
 import { addDays } from '@jarvis-network/core-utils/dist/base/date-time-utils';
-import { parseSupportedNetworkId } from '@jarvis-network/synthereum-ts/dist/config';
 import { loadRealm } from '@jarvis-network/synthereum-ts/dist/core/realms/synthereum/load';
 import { assertIsAddress } from '@jarvis-network/core-utils/dist/eth/address';
 import { Filter } from 'web3-eth-contract/types';
@@ -12,93 +9,87 @@ import {
   wei,
 } from '@jarvis-network/core-utils/dist/base/big-number';
 
-import {
-  log,
-  logTable,
-  logError,
-} from '@jarvis-network/core-utils/dist/logging';
+import { log, logTable } from '@jarvis-network/core-utils/dist/logging';
 import { assertNotNull } from '@jarvis-network/core-utils/dist/base/asserts';
+
+import { createCliApp } from './common/create-cli-app';
+import { buildCli } from './common/cli-config';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config();
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { hideBin } = require('yargs/helpers');
 
-const { argv } = yargs(hideBin(process.argv)).option('address', {
-  type: 'string',
-});
-
-async function main() {
-  log('Starting');
-  const netId = parseSupportedNetworkId(42);
-  const web3 = getInfuraWeb3(netId, 'wss');
-  log('Getting starting block');
-  const blockFrom250daysAgo = await getClosestBlock(
-    web3,
-    10000000,
-    addDays(new Date(), -250).getTime() / 1000,
-  );
-  log('Got starting block:', { blockFrom250daysAgo });
-  const realm = await loadRealm(web3, netId);
-  log('Realm loaded:', { poolRegistry: realm.poolRegistry!.address });
-  const myAddress = assertIsAddress<42>(
-    argv.address ?? '0x6e30001f52C69948066Afd91B417a988c543d3F1',
-  );
-  const fromBlock = blockFrom250daysAgo;
-  const toBlock = 'latest';
-
-  log('Getting ERC20 Transfer events for:', {
-    address: myAddress,
-    fromBlock,
-    toBlock,
-  });
-
-  const filters = [
-    {
-      from: [myAddress as string],
+createCliApp(
+  buildCli(__filename).option('address', {
+    type: 'string',
+    coerce: address => {
+      const returnAddr = assertIsAddress<42>(
+        address || '0x6e30001f52C69948066Afd91B417a988c543d3F1',
+      );
+      return returnAddr;
     },
-    {
-      to: [myAddress as string],
-    },
-  ] as Filter[];
+  }),
+  async ({ web3, netId, argv: { address } }) => {
+    log('Getting starting block');
+    const blockFrom250daysAgo = await getClosestBlock(
+      web3,
+      10000000,
+      addDays(new Date(), -250).getTime() / 1000,
+    );
+    log('Got starting block:', { blockFrom250daysAgo });
+    const realm = await loadRealm(web3, netId);
+    log('Realm loaded:', { poolRegistry: realm.poolRegistry!.address });
 
-  const erc20 = assertNotNull(realm.pools!.v4?.jEUR).collateralToken.instance;
+    const fromBlock = blockFrom250daysAgo;
+    const toBlock = 'latest';
 
-  const events = (
-    await Promise.all(
-      filters.map(filter =>
-        erc20.getPastEvents('Transfer', {
-          filter,
-          fromBlock,
-          toBlock,
+    log('Getting ERC20 Transfer events for:', {
+      address,
+      fromBlock,
+      toBlock,
+    });
+
+    const filters = [
+      {
+        from: [address as string],
+      },
+      {
+        to: [address as string],
+      },
+    ] as Filter[];
+
+    const erc20 = assertNotNull(realm.pools!.v4?.jEUR).collateralToken.instance;
+
+    const events = (
+      await Promise.all(
+        filters.map(filter =>
+          erc20.getPastEvents('Transfer', {
+            filter,
+            fromBlock,
+            toBlock,
+          }),
+        ),
+      )
+    ).flatMap(x => x);
+
+    const formatAddr = (addr: string) =>
+      `0x${addr.slice(2, 6)}..${addr.slice(-4)}`;
+
+    logTable(
+      events.map(
+        ({
+          transactionHash,
+          blockNumber,
+          returnValues: { from, to, value },
+        }) => ({
+          blockNumber,
+          from: formatAddr(from),
+          to: formatAddr(to),
+          value: `${formatAmount(
+            wei(value).mul(wei(10 ** 12)) as Amount,
+          )} USDC`,
+          transactionHash,
         }),
       ),
-    )
-  ).flatMap(x => x);
-
-  const formatAddr = (addr: string) =>
-    `0x${addr.slice(2, 6)}..${addr.slice(-4)}`;
-
-  logTable(
-    events.map(
-      ({
-        transactionHash,
-        blockNumber,
-        returnValues: { from, to, value },
-      }) => ({
-        blockNumber,
-        from: formatAddr(from),
-        to: formatAddr(to),
-        value: `${formatAmount(wei(value).mul(wei(10 ** 12)) as Amount)} USDC`,
-        transactionHash,
-      }),
-    ),
-  );
-}
-
-main()
-  .then(_ => process.exit(0))
-  .catch(err => {
-    logError(err);
-    process.exit(1);
-  });
+    );
+  },
+);
