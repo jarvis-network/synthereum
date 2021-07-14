2dc522ab625ead504ddfab20a845a9b39111950b
Alessandro Bergamaschi <alessandro@jarvis.network>
2021-07-14 11:25:34 +0200

    improve(polygon): Update configuarion of polygon package

--- libs/polygon/src/lib/polygon.spec.ts
+++
@@ -1,7 +0,0 @@
-import { polygon } from './polygon';
-
-describe('polygon', () => {
-  it('should work', () => {
-    expect(polygon()).toEqual('polygon');
-  });
-});
--- libs/polygon/src/lib/polygon.ts
+++
@@ -1,3 +0,0 @@
-export function polygon(): string {
-  return 'polygon';
-}
