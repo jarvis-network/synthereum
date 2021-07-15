0bece15e54ad7281cc48c297807335bc13debb38
Alessandro Bergamaschi <alessandro@jarvis.network>
2021-07-15 13:38:30 +0200

    improve(contracts): Fixes and optimizations of FixedRateWrapper

--- libs/legacy-currency-contracts/contracts/Maastricht/Maastricht.sol
+++ libs/legacy-currency-contracts/contracts/Maastricht/Maastricht.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 import '../FixedRateWrapper.sol';
 
 contract Maastricht is FixedRateWrapper {
-  constructor(IERC20 _token, uint256 _rate)
-    FixedRateWrapper(_token, _rate, 'Jarvis Synthetic Franc', 'jFRF')
+  constructor(IERC20 _token)
+    FixedRateWrapper(_token, 6559570 * 1e12, 'Jarvis Synthetic Franc', 'jFRF')
   {}
 }
