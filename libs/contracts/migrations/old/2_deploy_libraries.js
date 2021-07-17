49c7eea4085ea436574ed0a251be258bf67c933d
Alessandro Bergamaschi <alessandro@jarvis.network>
2021-07-17 16:07:53 +0200

    improve(contracts):  Removal of old unused migration and test scripts

--- libs/contracts/migrations/old/2_deploy_libraries.js
+++
@@ -1,8 +0,0 @@
-var TICHelper = artifacts.require('TICHelper');
-var TIC = artifacts.require('TIC');
-var TICFactory = artifacts.require('TICFactory');
-
-module.exports = function (deployer, network) {
-  deployer.deploy(TICHelper);
-  deployer.link(TICHelper, [TIC, TICFactory]);
-};
