342d6a33b43d25976507ae4122fbf08626aa2592
Andrea Di Nenno <dinennoandrea@gmail.com>
2021-07-20 18:02:07 +0200

    feat(contracts): :sparkles: Fixed rate currency contract
    Fixed rate currency contract

---
+++ libs/contracts/contracts/contracts/fixed-rate-currency/FixedRateCurrency.sol
@@ -0,0 +1,176 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.12;
+pragma experimental ABIEncoderV2;
+
+import './FixedRateWrapper.sol';
+import {ISynthereumFinder} from '../core/interfaces/IFinder.sol';
+import {ISynthereumRegistry} from '../core/registries/interfaces/IRegistry.sol';
+import {SynthereumInterfaces} from '../core/Constants.sol';
+import {
+  ISynthereumPoolOnChainPriceFeed
+} from '../synthereum-pool/v4/interfaces/IPoolOnChainPriceFeed.sol';
+
+contract FixedRateCurrency is FixedRateWrapper {
+  // Prevents minting when true
+  // TODO pause() function
+  bool public paused;
+
+  ISynthereumFinder public synthereumFinder;
+  ISynthereumPoolOnChainPriceFeed public synthereumPool; // pegSynth-USDC pool
+  IERC20 public collateralInstance; // synthereum pool collateral (USDC)
+
+  //----------------------------------------
+  // Events
+  //----------------------------------------
+  event Mint(address indexed account, address indexed pool, uint256 numTokens);
+
+  event Redeem(
+    address indexed account,
+    address indexed pool,
+    address indexed tokenRedeemed,
+    uint256 numTokensRedeemed
+  );
+
+  //----------------------------------------
+  // Modifiers
+  //----------------------------------------
+
+  modifier isActive() {
+    require(!paused, 'Contract has been paused');
+    _;
+  }
+
+  constructor(
+    IERC20 _pegToken,
+    ISynthereumPoolOnChainPriceFeed _synthereumPoolAddress,
+    ISynthereumFinder _synthereumFinder,
+    uint256 _rate,
+    string memory _name,
+    string memory _symbol
+  ) public FixedRateWrapper(_pegToken, _rate, _name, _symbol) {
+    paused = false;
+    synthereumFinder = _synthereumFinder;
+    synthereumPool = _synthereumPoolAddress;
+
+    address synthInstance;
+
+    // // check the appropriate pool is passed
+    // (, synthInstance) = checkPoolRegistration();
+
+    // require(address(_pegToken) == synthInstance, "The synth pool passed doesn't hold the peg token");
+  }
+
+  /** @notice - Mints fixed rate synths against the deposited peg synth (jEUR)
+   */
+  function mintFromPegSynth(uint256 _pegTokenAmount) public isActive() {
+    // approves this contract to pull peg tokens from user wallet
+    synth.safeApprove(address(this), _pegTokenAmount);
+
+    // deposit peg tokens and mint this token according to rate
+    uint256 numTokensMinted = super.wrap(_pegTokenAmount);
+
+    emit Mint(msg.sender, address(synthereumPool), numTokensMinted);
+  }
+
+  /** @notice - Burns fixed rate synths and unlocks the deposited peg synth (jEUR)
+   */
+  function redeemToPegSynth(uint256 _fixedSynthAmount) public {
+    uint256 tokenRedeemed = super.unwrap(_fixedSynthAmount);
+    emit Redeem(
+      msg.sender,
+      address(synthereumPool),
+      address(synth),
+      tokenRedeemed
+    );
+  }
+
+  /** @notice - Mints fixed rate synths from USDC.
+        Mints _pegToken from USDC using the appropriate synthereum pool.
+     */
+  function mintFromUSDC(
+    ISynthereumPoolOnChainPriceFeed.MintParams memory _mintParams
+  ) public isActive() {
+    // approves the pool to transfer collateral from user wallet
+    collateralInstance.safeApprove(
+      address(synthereumPool),
+      _mintParams.collateralAmount
+    );
+
+    // mint jEUR (peg token) with USDC - to user's wallet
+    (uint256 pegTokensMinted, ) = synthereumPool.mint(_mintParams);
+
+    // approves this contract to pull peg tokens from user wallet
+    synth.safeApprove(address(this), pegTokensMinted);
+
+    // wrap the jEUR to obtain this fixed rate currency
+    uint256 numTokensMinted = super.wrap(pegTokensMinted);
+
+    emit Mint(msg.sender, address(synthereumPool), numTokensMinted);
+  }
+
+  /** @notice - Redeem USDC by burning peg synth (from synthereum pool)
+        Unlocked by burning fixed rate synths (unwrap).
+     */
+  function redeemUSDC(
+    ISynthereumPoolOnChainPriceFeed.RedeemParams memory _redeemParams
+  ) public {
+    // burn _synthAmount to get peg tokens jEur
+    uint256 pegTokensUnlocked = super.unwrap(_redeemParams.numTokens);
+
+    // approves the synthereum pool to pull peg tokens tokens from user wallet
+    synth.safeApprove(address(synthereumPool), pegTokensUnlocked);
+
+    // redeem USDC by burning jEur in broker contract
+    _redeemParams.recipient = msg.sender;
+    (uint256 collateralRedeemed, ) = synthereumPool.redeem(_redeemParams);
+
+    emit Redeem(
+      msg.sender,
+      address(synthereumPool),
+      address(collateralInstance),
+      collateralRedeemed
+    );
+  }
+
+  function mintFromSynth(uint256 _synthAmount, address _synthAddress) public {
+    // exchange function in broker to get jEur from synth
+    // wrap jEur to get fixed rate synth
+  }
+
+  function swapForSynth(uint256 _fixedSynthAmount, address _synthAddress)
+    public
+  {
+    // unwrap fixedSynthAmount to get jEur
+    // exchange in broker for _synthAddress
+  }
+
+  // function checkPoolRegistration()
+  //     internal
+  //     view
+  //     returns (address collateralAddress, address synthInstance)
+  // {
+  //     ISynthereumRegistry poolRegistry =
+  //         ISynthereumRegistry(
+  //             synthereumFinder.getImplementationAddress(
+  //                 SynthereumInterfaces.PoolRegistry
+  //             )
+  //         );
+
+  //     string memory synthTokenSymbol = synthereumPool.syntheticTokenSymbol();
+  //     collateralInstance = synthereumPool.collateralToken();
+  //     uint8 version = synthereumPool.version();
+  //     require(
+  //     poolRegistry.isDeployed(
+  //         synthTokenSymbol,
+  //         collateralInstance,
+  //         version,
+  //         address(synthereumPool)
+  //     ),
+  //     'Pool not registred'
+  //     );
+
+  //     synthInstance = address(synthereumPool.syntheticToken());
+  //     collateralAddress = address(collateralInstance);
+  // }
+}
---
+++ libs/contracts/contracts/contracts/fixed-rate-currency/FixedRateWrapper.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.12;
+
+import '@openzeppelin/contracts/math/SafeMath.sol';
+import '@openzeppelin/contracts/token/ERC20/SafeERC20.sol';
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+
+contract FixedRateWrapper is ERC20 {
+  using SafeMath for uint256;
+  using SafeERC20 for IERC20;
+
+  // Precision for math operations
+  uint256 public constant PRECISION = 1e18;
+
+  // The synthetic token to which the legacy currency is pegged
+  IERC20 public immutable synth;
+
+  // The rate at which the conversion happens
+  uint256 public immutable rate;
+
+  // A variable to track total deposited amount of synthetic tokens
+  uint256 public total_deposited;
+
+  /** @notice Constructs the ERC20 contract and sets the immutable variables
+   * @param _token - The synthetic token to which the legacy currency is pegged
+   * @param _rate - The rate at which the conversion happens
+   * @param _name - The name of the synthetic legacy currency token
+   * @param _symbol - The symbol of the synthetic legacy currency token
+   */
+  constructor(
+    IERC20 _token,
+    uint256 _rate,
+    string memory _name,
+    string memory _symbol
+  ) public ERC20(_name, _symbol) {
+    synth = _token;
+    rate = _rate;
+  }
+
+  /** @notice - A public function which takes synthetic tokens as deposit and mints legacy synthetic tokens for the user
+   * @param _amount - The amount of synthetic tokens the user wants to deposit
+   */
+  function wrap(uint256 _amount) public returns (uint256 amountTokens) {
+    amountTokens = _amount.mul(rate).div(PRECISION);
+    synth.safeTransferFrom(msg.sender, address(this), _amount);
+    total_deposited = total_deposited.add(_amount);
+    _mint(msg.sender, amountTokens);
+  }
+
+  /** @notice - A public function which takes legacy synthetic tokens as deposit and burns them, releasing synthetic tokens to the user
+   * @param _amount - The amount of legacy synthetic tokens the user wants to deposit and burn
+   */
+  function unwrap(uint256 _amount) public returns (uint256 amountTokens) {
+    require(balanceOf(msg.sender) >= _amount, 'Not enought tokens to unwrap');
+    amountTokens = total_deposited.mul(_amount).div(totalSupply());
+    _burn(msg.sender, _amount);
+    total_deposited = total_deposited.sub(amountTokens);
+    synth.safeTransfer(msg.sender, amountTokens);
+  }
+}
