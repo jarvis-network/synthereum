32f7532525e0c80b4d9f2a2d0d1484bdb69500b0
Alessandro Bergamaschi <alessandro@jarvis.network>
2021-07-28 00:26:00 +0200

    feat(contracts): Set synthtic token with permit as default factory for polygon and local tests

--- libs/contracts/migrations/07_deploy_tokenFactory.js
+++ libs/contracts/migrations/07_deploy_tokenFactory.js
@@ -1,6 +1,7 @@
 module.exports = require('../utils/getContractsFactory')(migrate, [
   'SynthereumFinder',
   'SynthereumSyntheticTokenFactory',
+  'SynthereumSyntheticTokenPermitFactory',
 ]);
 
 async function migrate(deployer, network, accounts) {
@@ -9,7 +10,11 @@ async function migrate(deployer, network, accounts) {
   const {
     SynthereumFinder,
     SynthereumSyntheticTokenFactory,
+    SynthereumSyntheticTokenPermitFactory,
   } = migrate.getContracts(artifacts);
+  const {
+    isPublicNetwork,
+  } = require('@jarvis-network/hardhat-utils/dist/deployment/migrationUtils');
   const {
     getKeysForNetwork,
     deploy,
@@ -22,24 +27,40 @@ async function migrate(deployer, network, accounts) {
   const maintainer = rolesConfig[networkId]?.maintainer ?? accounts[1];
   const synthereumFinder = await getExistingInstance(web3, SynthereumFinder);
   const keys = getKeysForNetwork(network, accounts);
-  await deploy(
-    web3,
-    deployer,
-    network,
-    SynthereumSyntheticTokenFactory,
-    synthereumFinder.options.address,
-    { from: keys.deployer },
-  );
+  let tokenFactory;
+  if (!isPublicNetwork(network) || networkId == 80001 || networkId === 137) {
+    await deploy(
+      web3,
+      deployer,
+      network,
+      SynthereumSyntheticTokenPermitFactory,
+      synthereumFinder.options.address,
+      { from: keys.deployer },
+    );
+    tokenFactory = await getExistingInstance(
+      web3,
+      SynthereumSyntheticTokenPermitFactory,
+    );
+  } else {
+    await deploy(
+      web3,
+      deployer,
+      network,
+      SynthereumSyntheticTokenFactory,
+      synthereumFinder.options.address,
+      { from: keys.deployer },
+    );
+    tokenFactory = await getExistingInstance(
+      web3,
+      SynthereumSyntheticTokenFactory,
+    );
+  }
   const tokenFactoryInterface = await web3.utils.stringToHex('TokenFactory');
-  const tokenFactory = await getExistingInstance(
-    web3,
-    SynthereumSyntheticTokenFactory,
-  );
   await synthereumFinder.methods
     .changeImplementationAddress(
       tokenFactoryInterface,
       tokenFactory.options.address,
     )
     .send({ from: maintainer });
-  console.log('SynthereumSyntheticTokenFactory added to SynthereumFinder');
+  console.log('Token Factory added to SynthereumFinder');
 }
