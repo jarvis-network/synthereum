4196b73d1d394db93f80453b7e3c8b43e273e84c
Alessandro Bergamaschi <alessandro@jarvis.network>
2021-08-20 15:29:17 +0200

    feat(atomic-swap): Update atomic-swap package

--- libs/atomic-swap/src/lib/atomic-swap.spec.ts
+++
@@ -1,7 +0,0 @@
-import { atomicSwap } from './atomic-swap';
-
-describe('atomicSwap', () => {
-  it('should work', () => {
-    expect(atomicSwap()).toEqual('atomic-swap');
-  });
-});
--- libs/atomic-swap/src/lib/atomic-swap.ts
+++
@@ -1,3 +0,0 @@
-export function atomicSwap(): string {
-  return 'atomic-swap';
-}
