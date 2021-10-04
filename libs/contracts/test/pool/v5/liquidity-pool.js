dd0929deaa1a35f0608f32bab62905b6da2ad6be
Alessandro Bergamaschi <alessandro@jarvis.network>
2021-10-04 12:14:41 +0200

    test(contracts): Add test for reverting conditions in the constructor

--- libs/contracts/test/pool/v5/liquidity-pool.js
+++ libs/contracts/test/pool/v5/liquidity-pool.js
@@ -1,4 +1,5 @@
 const web3Utils = require('web3-utils');
+const truffleAssert = require('truffle-assertions');
 const SynthereumFinder = artifacts.require('SynthereumFinder');
 const TestnetERC20 = artifacts.require('TestnetERC20');
 const MintableBurnableSyntheticTokenPermit = artifacts.require(
@@ -47,6 +48,11 @@ contract('LiquidityPool', function (accounts) {
   const liquidationReward = web3Utils.toWei('0.75');
   const synthTokenSymbol = 'jEUR';
 
+  before(async () => {
+    liquidityPoolLibInstance = await SynthereumLiquidityPoolLib.new();
+    await SynthereumLiquidityPool.link(liquidityPoolLibInstance);
+  });
+
   beforeEach(async () => {
     collateralInstance = await TestnetERC20.deployed();
     collateralAddress = collateralInstance.address;
@@ -59,8 +65,6 @@ contract('LiquidityPool', function (accounts) {
     synthTokenAddress = synthTokenInstance.address;
     finderInstance = await SynthereumFinder.deployed();
     finderAddress = finderInstance.address;
-    liquidityPoolLibInstance = await SynthereumLiquidityPoolLib.new();
-    await SynthereumLiquidityPool.link(liquidityPoolLibInstance);
     liquidityPoolInstance = await SynthereumLiquidityPool.new(
       finderAddress,
       version,
@@ -142,6 +146,166 @@ contract('LiquidityPool', function (accounts) {
         feeInfo[2].toString(),
         web3Utils.toBN(feeTotalProportion).toString(),
         'Wrong fee total proportion initialization',
+      );
+    });
+    it('Can revert if collateral requirement is less than 100% ', async () => {
+      const wrongCollateralRequirement = web3Utils.toWei('0.999');
+      await truffleAssert.reverts(
+        SynthereumLiquidityPool.new(
+          finderAddress,
+          version,
+          collateralAddress,
+          synthTokenAddress,
+          roles,
+          overCollateralization,
+          fee,
+          priceFeedIdentifier,
+          wrongCollateralRequirement,
+          liquidationReward,
+        ),
+        'Collateral requirement must be bigger than 100%',
+      );
+    });
+    it('Can revert if overCollateralization is less then Lp part of the collateral', async () => {
+      const wrongOverCollateralization = web3Utils.toWei('0.03');
+      await truffleAssert.reverts(
+        SynthereumLiquidityPool.new(
+          finderAddress,
+          version,
+          collateralAddress,
+          synthTokenAddress,
+          roles,
+          wrongOverCollateralization,
+          fee,
+          priceFeedIdentifier,
+          collateralRequirement,
+          liquidationReward,
+        ),
+        'Overcollateralization must be bigger than the Lp part of the collateral requirement',
+      );
+    });
+    it('Can revert if liquidation reward is 0', async () => {
+      await truffleAssert.reverts(
+        SynthereumLiquidityPool.new(
+          finderAddress,
+          version,
+          collateralAddress,
+          synthTokenAddress,
+          roles,
+          overCollateralization,
+          fee,
+          priceFeedIdentifier,
+          collateralRequirement,
+          0,
+        ),
+        'Liquidation reward must be between 0 and 100%',
+      );
+    });
+    it('Can revert if liquidation reward is bigger than 100%', async () => {
+      const wrongLiquidationReward = web3Utils.toWei('1.01');
+      await truffleAssert.reverts(
+        SynthereumLiquidityPool.new(
+          finderAddress,
+          version,
+          collateralAddress,
+          synthTokenAddress,
+          roles,
+          overCollateralization,
+          fee,
+          priceFeedIdentifier,
+          collateralRequirement,
+          wrongLiquidationReward,
+        ),
+        'Liquidation reward must be between 0 and 100%',
+      );
+    });
+    it('Can revert if collateral has more than 18 decimals', async () => {
+      const wrongCollateralToken = await TestnetERC20.new(
+        'Test token',
+        'TEST',
+        20,
+      );
+      await truffleAssert.reverts(
+        SynthereumLiquidityPool.new(
+          finderAddress,
+          version,
+          wrongCollateralToken.address,
+          synthTokenAddress,
+          roles,
+          overCollateralization,
+          fee,
+          priceFeedIdentifier,
+          collateralRequirement,
+          liquidationReward,
+        ),
+        'Collateral has more than 18 decimals',
+      );
+    });
+    it('Can revert if synthetic token has more or less than 18 decimals', async () => {
+      let wrongSynthTokenInstance = await MintableBurnableSyntheticTokenPermit.new(
+        'Jarvis Synthetic Euro',
+        synthTokenSymbol,
+        16,
+        { from: admin },
+      );
+      let wrongSynthTokenAddress = wrongSynthTokenInstance.address;
+      await truffleAssert.reverts(
+        SynthereumLiquidityPool.new(
+          finderAddress,
+          version,
+          collateralAddress,
+          wrongSynthTokenAddress,
+          roles,
+          overCollateralization,
+          fee,
+          priceFeedIdentifier,
+          collateralRequirement,
+          liquidationReward,
+        ),
+        'Synthetic token has more or less than 18 decimals',
+      );
+      wrongSynthTokenInstance = await MintableBurnableSyntheticTokenPermit.new(
+        'Jarvis Synthetic Euro',
+        synthTokenSymbol,
+        20,
+        { from: admin },
+      );
+      wrongSynthTokenAddress = wrongSynthTokenInstance.address;
+      await truffleAssert.reverts(
+        SynthereumLiquidityPool.new(
+          finderAddress,
+          version,
+          collateralAddress,
+          wrongSynthTokenAddress,
+          roles,
+          overCollateralization,
+          fee,
+          priceFeedIdentifier,
+          collateralRequirement,
+          liquidationReward,
+        ),
+        'Synthetic token has more or less than 18 decimals',
+      );
+    });
+    it('Can revert if price identifier is not supported by the the price feed', async () => {
+      const wrongPriceIdentifier = web3Utils.padRight(
+        web3Utils.toHex('EUR/NOT-USD'),
+        64,
+      );
+      await truffleAssert.reverts(
+        SynthereumLiquidityPool.new(
+          finderAddress,
+          version,
+          collateralAddress,
+          synthTokenAddress,
+          roles,
+          overCollateralization,
+          fee,
+          wrongPriceIdentifier,
+          collateralRequirement,
+          liquidationReward,
+        ),
+        'Price identifier not supported',
       );
     });
   });
