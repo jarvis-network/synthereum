2c7ff0d687ae31b3621f76e0ee5a2d952dc07c9c
Dimitar Nestorov <dimitar.nestorov@jarvis.network>
2021-10-05 15:31:10 +0300

    Don't rely on `sending` event (doesn't seem to be emitted on MetaMask 10.1.0)

--- libs/core-utils/src/eth/contracts/send-tx.ts
+++ libs/core-utils/src/eth/contracts/send-tx.ts
@@ -178,14 +178,15 @@ export async function logTransactionStatus<T, Net extends NetworkName>({
   confirmations?: number;
 }) {
   await once(promiEvent, 'sent');
-  log('  [1/4] Sending tx...');
+  log('  [1/3] Sending tx...');
 
-  await once(promiEvent, 'sending');
-  log('  [2/4] Tx sent. Waiting for hash...');
+  once(promiEvent, 'sending').then(() => {
+    log('  Tx sent. Waiting for hash...');
+  });
 
   const txHash = await once(promiEvent, 'transactionHash');
   log(
-    `  [3/4]: Tx hash: '${txHash}'. Waiting for ${confirmations} confirmations...`,
+    `  [2/3]: Tx hash: '${txHash}'. Waiting for ${confirmations} confirmations...`,
   );
 
   const [confirmation, receipt] = await once(
@@ -195,7 +196,7 @@ export async function logTransactionStatus<T, Net extends NetworkName>({
   );
   const { gasUsed, blockNumber } = receipt;
   log(
-    `  [4/4]: Tx confirmed ${confirmation} time(s). Gas used: ${gasUsed} | block number: ${blockNumber}`,
+    `  [3/3]: Tx confirmed ${confirmation} time(s). Gas used: ${gasUsed} | block number: ${blockNumber}`,
   );
 
   return receipt;
