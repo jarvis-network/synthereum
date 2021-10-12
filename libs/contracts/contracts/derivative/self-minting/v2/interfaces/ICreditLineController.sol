00805618dc8dcaa4fbcd4be78ac72cc4bc13816f
Andrea Di Nenno <dinennoandrea@gmail.com>
2021-10-12 12:44:49 +0200

    feat(contracts): :sparkles: Credit Line controller contract and iface
    Credit Line controller contract and iface to set dynamic params

--- libs/contracts/contracts/derivative/self-minting/v2/interfaces/ICreditLineController.sol
+++ libs/contracts/contracts/derivative/self-minting/v2/interfaces/ICreditLineController.sol
@@ -2,16 +2,14 @@
 pragma solidity ^0.8.4;
 
 import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import {
+  FixedPoint
+} from '@uma/core/contracts/common/implementation/FixedPoint.sol';
+import {ICreditLineStorage} from './ICreditLineStorage.sol';
 
 /** @title Interface for interacting with the SelfMintingController
  */
 interface ICreditLineController {
-  //Describe fee structure
-  struct DaoFee {
-    uint256 feePercentage;
-    address feeRecipient;
-  }
-
   /**
    * @notice Allow to set capMintAmount on a list of registered self-minting derivatives
    * @param selfMintingDerivatives Self-minting derivatives
@@ -23,44 +21,46 @@ interface ICreditLineController {
   ) external;
 
   /**
-   * @notice Allow to set capDepositRatio on a list of registered self-minting derivatives
-   * @param selfMintingDerivatives Self-minting derivatives
-   * @param capDepositRatios Deposit caps ratios for self-minting derivatives
-   */
-  function setCapDepositRatio(
-    address[] calldata selfMintingDerivatives,
-    uint256[] calldata capDepositRatios
-  ) external;
-
-  /**
-   * @notice Allow to set Dao fees on a list of registered self-minting derivatives
-   * @param selfMintingDerivatives Self-minting derivatives
-   * @param daoFees Dao fees for self-minting derivatives
-   */
-  function setDaoFee(
-    address[] calldata selfMintingDerivatives,
-    DaoFee[] calldata daoFees
-  ) external;
-
-  /**
    * @notice Allow to set Dao fee percentages on a list of registered self-minting derivatives
    * @param selfMintingDerivatives Self-minting derivatives
-   * @param daoFeePercentages Dao fee percentages for self-minting derivatives
+   * @param feePercentages fee percentages for self-minting derivatives
    */
-  function setDaoFeePercentage(
+  function setFeePercentage(
     address[] calldata selfMintingDerivatives,
-    uint256[] calldata daoFeePercentages
+    FixedPoint.Unsigned[] calldata feePercentages
   ) external;
 
   /**
-   * @notice Allow to set Dao fee recipients on a list of registered self-minting derivatives
-   * @param selfMintingDerivatives Self-minting derivatives
-   * @param daoFeeRecipients Dao fee recipients for self-minting derivatives
+   * @notice Update the addresses of recipients for generated fees and proportions of fees each address will receive
+   * @param selfMintingDerivatives Derivatives to update
+   * @param feeRecipients An array of the addresses of recipients that will receive generated fees
+   * @param feeProportions An array of the proportions of fees generated each recipient will receive
    */
-  function setDaoFeeRecipient(
+  function setFeeRecipients(
     address[] calldata selfMintingDerivatives,
-    address[] calldata daoFeeRecipients
+    address[][] calldata feeRecipients,
+    uint32[][] calldata feeProportions
   ) external;
+
+  /**
+   * @notice Update the liquidation reward percentage
+   * @param selfMintingDerivatives Derivatives to update
+   * @param _liquidationRewards Percentage of reward for correct liquidation by a liquidator
+   */
+  function setLiquidationRewardPercentage(
+    address[] calldata selfMintingDerivatives,
+    FixedPoint.Unsigned[] calldata _liquidationRewards
+  ) external;
+
+  /**
+   * @notice Gets the set liquidtion reward percentage of a self-minting derivative
+   * @param selfMintingDerivative Self-minting derivative
+   * @return liquidation Reward percentage
+   */
+  function getLiquidationRewardPercentage(address selfMintingDerivative)
+    external
+    view
+    returns (uint256);
 
   /**
    * @notice Gets the set CapMintAmount of a self-minting derivative
@@ -73,42 +73,12 @@ interface ICreditLineController {
     returns (uint256 capMintAmount);
 
   /**
-   * @notice Gets the set CapDepositRatio of a self-minting derivative
+   * @notice Gets the fee params of a self-minting derivative
    * @param selfMintingDerivative Self-minting derivative
-   * @return capDepositRatio Limit ratio for a user deposit
+   * @return fee fee info (percent + recipient + proportions)
    */
-  function getCapDepositRatio(address selfMintingDerivative)
+  function getFeeInfo(address selfMintingDerivative)
     external
     view
-    returns (uint256 capDepositRatio);
-
-  /**
-   * @notice Gets the set DAO fee of a self-minting derivative
-   * @param selfMintingDerivative Self-minting derivative
-   * @return daoFee Dao fee info (percent + recipient)
-   */
-  function getDaoFee(address selfMintingDerivative)
-    external
-    view
-    returns (DaoFee memory daoFee);
-
-  /**
-   * @notice Gets the set DAO fee percentage of a self-minting derivative
-   * @param selfMintingDerivative Self-minting derivative
-   * @return daoFeePercentage Dao fee percent
-   */
-  function getDaoFeePercentage(address selfMintingDerivative)
-    external
-    view
-    returns (uint256 daoFeePercentage);
-
-  /**
-   * @notice Gets the set DAO fee recipient of a self-minting derivative
-   * @param selfMintingDerivative Self-minting derivative
-   * @return recipient Dao fee recipient
-   */
-  function getDaoFeeRecipient(address selfMintingDerivative)
-    external
-    view
-    returns (address recipient);
+    returns (ICreditLineStorage.Fee memory fee);
 }
