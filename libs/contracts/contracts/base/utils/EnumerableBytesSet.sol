e841dbe30f766018a012fcb1be8de407d49e330e
Alessandro Bergamaschi <alessandro@jarvis.network>
2021-11-12 03:29:51 +0100

    feat(contracts): Add Synthereum identifier whitelist

--- libs/contracts/contracts/base/utils/EnumerableBytesSet.sol
+++ libs/contracts/contracts/base/utils/EnumerableBytesSet.sol
@@ -27,6 +27,40 @@ library EnumerableBytesSet {
     }
   }
 
+  function remove(BytesSet storage set, bytes32 value) internal returns (bool) {
+    // We read and store the value's index to prevent multiple reads from the same storage slot
+    uint256 valueIndex = set._indexes[value];
+
+    if (valueIndex != 0) {
+      // Equivalent to contains(set, value)
+      // To delete an element from the _values array in O(1), we swap the element to delete with the last one in
+      // the array, and then remove the last element (sometimes called as 'swap and pop').
+      // This modifies the order of the array, as noted in {at}.
+
+      uint256 toDeleteIndex = valueIndex - 1;
+      uint256 lastIndex = set._values.length - 1;
+
+      if (lastIndex != toDeleteIndex) {
+        bytes32 lastvalue = set._values[lastIndex];
+
+        // Move the last value to the index where the value to delete is
+        set._values[toDeleteIndex] = lastvalue;
+        // Update the index for the moved value
+        set._indexes[lastvalue] = valueIndex; // Replace lastvalue's index to valueIndex
+      }
+
+      // Delete the slot where the moved value was stored
+      set._values.pop();
+
+      // Delete the index for the deleted slot
+      delete set._indexes[value];
+
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   /**
    * @dev Returns true if the value is in the set. O(1).
    */
