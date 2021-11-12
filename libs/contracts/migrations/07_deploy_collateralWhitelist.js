8b9dfc2768f856de651b829a8eb17bccb3326202
Alessandro Bergamaschi <alessandro@jarvis.network>
2021-11-12 18:17:13 +0100

    feat(contracts): Add migration script for CollateralWhitelist

---
+++ libs/contracts/migrations/07_deploy_collateralWhitelist.js
@@ -0,0 +1,51 @@
+module.exports = require('../utils/getContractsFactory')(migrate, [
+  'SynthereumFinder',
+  'SynthereumCollateralWhitelist',
+]);
+
+async function migrate(deployer, network, accounts) {
+  const rolesConfig = require('../data/roles.json');
+  const {
+    getExistingInstance,
+  } = require('@jarvis-network/hardhat-utils/dist/deployment/get-existing-instance');
+  const {
+    SynthereumCollateralWhitelist,
+    SynthereumFinder,
+  } = migrate.getContracts(artifacts);
+  const {
+    getKeysForNetwork,
+    deploy,
+  } = require('@jarvis-network/hardhat-utils/dist/deployment/migrationUtils');
+  const {
+    toNetworkId,
+  } = require('@jarvis-network/core-utils/dist/eth/networks');
+
+  const networkId = toNetworkId(network);
+  const synthereumFinder = await getExistingInstance(
+    web3,
+    SynthereumFinder,
+    '@jarvis-network/synthereum-contracts',
+  );
+  const admin = rolesConfig[networkId]?.admin ?? accounts[0];
+  const maintainer = rolesConfig[networkId]?.maintainer ?? accounts[1];
+  const roles = { admin: admin, maintainer: maintainer };
+  const keys = getKeysForNetwork(network, accounts);
+  await deploy(web3, deployer, network, SynthereumCollateralWhitelist, roles, {
+    from: keys.deployer,
+  });
+  const collateralWhitelistInterface = await web3.utils.stringToHex(
+    'CollateralWhitelist',
+  );
+  const synthereumCollateralWhitelist = await getExistingInstance(
+    web3,
+    SynthereumCollateralWhitelist,
+    '@jarvis-network/synthereum-contracts',
+  );
+  await synthereumFinder.methods
+    .changeImplementationAddress(
+      collateralWhitelistInterface,
+      synthereumCollateralWhitelist.options.address,
+    )
+    .send({ from: maintainer });
+  console.log('SynthereumCollateralWhitelist added to SynthereumFinder');
+}
