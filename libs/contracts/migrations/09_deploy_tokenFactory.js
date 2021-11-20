958072cea2d1b0e494853e69968219f2ecb7e0f6
Alessandro Bergamaschi <alessandro@jarvis.network>
2021-11-20 20:40:32 +0100

    feat(contracts): Set SynthereumSyntheticTokenPermitFactory as default token factory for bsc and bsc-testnet

--- libs/contracts/migrations/09_deploy_tokenFactory.js
+++ libs/contracts/migrations/09_deploy_tokenFactory.js
@@ -34,7 +34,13 @@ async function migrate(deployer, network, accounts) {
   );
   const keys = getKeysForNetwork(network, accounts);
   let tokenFactory;
-  if (!isPublicNetwork(network) || networkId == 80001 || networkId === 137) {
+  if (
+    !isPublicNetwork(network) ||
+    networkId == 80001 ||
+    networkId === 137 ||
+    networkId === 56 ||
+    networkId === 97
+  ) {
     await deploy(
       web3,
       deployer,
