50d8d09c2d1d9777cf1adc48dfc1e39d2f3a5032
Fabien C <fabiencoutant.ctt@gmail.com>
2021-11-23 15:46:16 +0200

    improve(ui): :lipstick: Add stories using singleValueComponent & optionComponent

--- libs/ui/src/JSelect/stories/Select.stories.tsx
+++ libs/ui/src/JSelect/stories/Select.stories.tsx
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
 
-import { Select } from '..';
+import {
+  Select,
+  SingleValueProps,
+  SingleValue,
+  Option as OptionWrapper,
+  OptionProps,
+} from '..';
 import { styled } from '../../Theme';
 
 export default {
@@ -80,6 +86,87 @@ export const CustomizedSelect = () => {
         options={['USDC', 'jEUR', 'jGBP']}
       />
       Selected value: {value}
+    </div>
+  );
+};
+
+const CustomSelect = styled(Select as Select<Option>)`
+  .react-select__control {
+    background: #ff8585;
+    max-height: none;
+  }
+  .react-select__single-value {
+    font-size: 24px;
+    padding: 10px;
+  }
+`;
+const AlignVertically = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 4px 0;
+`;
+
+export const multiSelect = () => {
+  const [values, setValues] = useState(['jEUR']);
+
+  const options = ['USDC', 'jEUR', 'jGBP'].map<Option>(val => ({
+    value: val,
+    label: `${val} token`,
+    icon: <div style={{ marginRight: '16px' }}>ICON</div>,
+  }));
+
+  const handleSelectChange = (symbols: string) => {
+    if (values.includes(symbols)) {
+      setValues(values.filter(value => value !== symbols));
+    } else {
+      setValues(oldValues => [...oldValues, symbols]);
+    }
+  };
+
+  const renderSelectedValues = (): string => {
+    if (!values.length) return '';
+    if (values.length === 1) return values[0];
+    return values.join(', ');
+  };
+
+  const OptionComponent = (props: OptionProps<Option, false>) => {
+    const {
+      data: { icon, label },
+    } = props;
+    return (
+      <OptionWrapper {...props}>
+        <AlignVertically>
+          {icon}
+          {label}
+        </AlignVertically>
+      </OptionWrapper>
+    );
+  };
+  const SelectValue = (props: SingleValueProps<Option>) => {
+    const {
+      data: { icon, label },
+    } = props;
+    return (
+      <SingleValue {...props}>
+        <AlignVertically>
+          {icon}
+          {label}
+        </AlignVertically>
+      </SingleValue>
+    );
+  };
+
+  return (
+    <div style={{ width: '300px' }}>
+      <CustomSelect
+        selected={renderSelectedValues()}
+        onChange={(val: any) => handleSelectChange(String(val!.value))}
+        rowsText=""
+        options={options}
+        optionComponent={OptionComponent}
+        singleValueComponent={SelectValue}
+      />
+      Selected values: {renderSelectedValues()}
     </div>
   );
 };
