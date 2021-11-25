5dfdc3d9a3452e6b7e89a94649227ea5174c9523
Andrea Di Nenno <dinennoandrea@gmail.com>
2021-11-25 15:24:00 +0100

    test(contracts): :white_check_mark: Add pool v5 in testing
    Add pool v5 in testing

--- libs/atomic-swap/contracts/v3/test/Import.sol
+++ libs/atomic-swap/contracts/v3/test/Import.sol
@@ -3,3 +3,6 @@
 pragma solidity ^0.8.4;
 
 import '@openzeppelin/contracts/metatx/MinimalForwarder.sol';
+import {
+  SynthereumLiquidityPool
+} from '@jarvis-network/synthereum-contracts/contracts/synthereum-pool/v5/LiquidityPool.sol';
