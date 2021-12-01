9529f4749f128b541ec76f1d1115c9417ecc4f19
Andrea Di Nenno <dinennoandrea@gmail.com>
2021-12-01 09:23:51 +0100

    test(contracts): :white_check_mark: Add hardhat ethers and signature utils
    Add hardhat ethers and signature utils

--- libs/atomic-swap/hardhat.config.ts
+++ libs/atomic-swap/hardhat.config.ts
@@ -10,6 +10,7 @@ import 'solidity-coverage';
 import 'hardhat-gas-reporter';
 import '@nomiclabs/hardhat-web3';
 import '@nomiclabs/hardhat-etherscan';
+import '@nomiclabs/hardhat-ethers';
 
 require('dotenv').config();
 
