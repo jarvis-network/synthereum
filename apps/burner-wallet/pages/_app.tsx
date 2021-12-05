630b612a89f7596caf05e8118a4a31056aa17e1d
Petar Kirov <petar.p.kirov@gmail.com>
2021-12-05 23:08:56 +0200

    fix(burner-wallet): :art: Fix issues reported by ESLint

--- apps/burner-wallet/pages/_app.tsx
+++ apps/burner-wallet/pages/_app.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
+import { FC } from 'react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+
 import { ReactComponent as NxLogo } from '../public/nx-logo-white.svg';
 import './styles.css';
 
-function CustomApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <title>Welcome to burner-wallet!</title>
-      </Head>
-      <div className="app">
-        <header className="flex">
-          <NxLogo width="75" height="50" />
-          <h1>Welcome to burner-wallet!</h1>
-        </header>
-        <main>
-          <Component {...pageProps} />
-        </main>
-      </div>
-    </>
-  );
-}
+const CustomApp: FC<AppProps> = ({ Component, pageProps }) => (
+  <>
+    <Head>
+      <title>Welcome to burner-wallet!</title>
+    </Head>
+    <div className="app">
+      <header className="flex">
+        <NxLogo width="75" height="50" />
+        <h1>Welcome to burner-wallet!</h1>
+      </header>
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </div>
+  </>
+);
 
 export default CustomApp;
