5f009b49a98ef941e2d92cc85b8ed36151e101ed
Alessandro Bergamaschi <alessandro@jarvis.network>
2022-01-03 17:27:14 +0100

    feat(contracts): Add support for xDAI network in hardhat.config

--- libs/contracts/hardhat.config.ts
+++ libs/contracts/hardhat.config.ts
@@ -94,5 +94,6 @@ addPublicNetwork(config, 80001, process.env.POLYGON_PROJECT_ID!);
 addPublicNetwork(config, 56, process.env.BSC_PROJECT_ID);
 addPublicNetwork(config, 97, process.env.BSC_PROJECT_ID);
 addPublicNetwork(config, 77);
+addPublicNetwork(config, 100);
 
 export default config;
