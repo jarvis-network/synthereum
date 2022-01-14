3966213cbb7924ff74e95403ec3a6093ef87cede
iliyaniliev <iliyan.iliev@jarvis.exchange>
2022-01-14 16:13:16 +0200

    feat(contracts): Add FixedRateV1 contracts

---
+++ libs/contracts/contracts/fixed-rate/v1/FixedRateCreator.sol
@@ -0,0 +1,141 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.4;
+
+import {IStandardERC20} from '../../base/interfaces/IStandardERC20.sol';
+import {
+  IMintableBurnableTokenFactory
+} from '../../tokens/factories/interfaces/IMintableBurnableTokenFactory.sol';
+import {ISynthereumFinder} from '../../core/interfaces/IFinder.sol';
+import {
+  IMintableBurnableERC20
+} from '../../tokens/interfaces/IMintableBurnableERC20.sol';
+import {
+  BaseControlledMintableBurnableERC20
+} from '../../tokens/interfaces/BaseControlledMintableBurnableERC20.sol';
+import {SynthereumInterfaces} from '../../core/Constants.sol';
+import {SynthereumFixedRateWrapper} from './FixedRateWrapper.sol';
+import {
+  ISynthereumFixedRateWrapperStorage
+} from './interfaces/IFixedRateWrapperStorage.sol';
+
+contract SynthereumFixedRateCreator {
+  struct Params {
+    IStandardERC20 collateralToken;
+    string syntheticName;
+    string syntheticSymbol;
+    address syntheticToken;
+    ISynthereumFixedRateWrapperStorage.Roles roles;
+    uint8 version;
+    uint256 rate;
+  }
+
+  // Address of Synthereum Finder
+  ISynthereumFinder public immutable synthereumFinder;
+
+  //----------------------------------------
+  // Events
+  //----------------------------------------
+  event CreatedFixedRate(
+    address indexed fixedRateAddress,
+    uint8 indexed version,
+    address indexed deployerAddress
+  );
+
+  //----------------------------------------
+  // Constructor
+  //----------------------------------------
+
+  /**
+   * @notice Constructs the FixedRateWrapper contract.
+   * @param _synthereumFinder Synthereum Finder address used to discover other contracts
+   */
+  constructor(address _synthereumFinder) {
+    synthereumFinder = ISynthereumFinder(_synthereumFinder);
+  }
+
+  //----------------------------------------
+  // Public functions
+  //----------------------------------------
+
+  /**
+   * @notice Creates an instance of the fixed rate
+   * @param params is a `ConstructorParams` object from FixedRateWrapper.
+   * @return fixedRate Address of the deployed fixedRate contract.
+   */
+  function createFixedRate(Params calldata params)
+    public
+    virtual
+    returns (SynthereumFixedRateWrapper fixedRate)
+  {
+    require(bytes(params.syntheticName).length != 0, 'Missing synthetic name');
+    require(
+      bytes(params.syntheticSymbol).length != 0,
+      'Missing synthetic symbol'
+    );
+
+    if (params.syntheticToken == address(0)) {
+      IMintableBurnableTokenFactory tokenFactory =
+        IMintableBurnableTokenFactory(
+          ISynthereumFinder(synthereumFinder).getImplementationAddress(
+            SynthereumInterfaces.TokenFactory
+          )
+        );
+      BaseControlledMintableBurnableERC20 tokenCurrency =
+        tokenFactory.createToken(
+          params.syntheticName,
+          params.syntheticSymbol,
+          18
+        );
+      fixedRate = new SynthereumFixedRateWrapper(
+        _convertParams(params, tokenCurrency)
+      );
+      // Give permissions to new pool contract and then hand over ownership.
+      tokenCurrency.addMinter(address(fixedRate));
+      tokenCurrency.addBurner(address(fixedRate));
+      tokenCurrency.addAdmin(
+        synthereumFinder.getImplementationAddress(SynthereumInterfaces.Manager)
+      );
+      tokenCurrency.renounceAdmin();
+    } else {
+      BaseControlledMintableBurnableERC20 tokenCurrency =
+        BaseControlledMintableBurnableERC20(params.syntheticToken);
+      require(
+        keccak256(abi.encodePacked(tokenCurrency.name())) ==
+          keccak256(abi.encodePacked(params.syntheticName)),
+        'Wrong synthetic token name'
+      );
+      require(
+        keccak256(abi.encodePacked(tokenCurrency.symbol())) ==
+          keccak256(abi.encodePacked(params.syntheticSymbol)),
+        'Wrong synthetic token symbol'
+      );
+      fixedRate = new SynthereumFixedRateWrapper(
+        _convertParams(params, tokenCurrency)
+      );
+    }
+    emit CreatedFixedRate(address(fixedRate), params.version, msg.sender);
+    return fixedRate;
+  }
+
+  // Converts createFixedRate params to constructor params.
+  function _convertParams(
+    Params memory params,
+    BaseControlledMintableBurnableERC20 tokenCurrency
+  )
+    internal
+    view
+    returns (
+      SynthereumFixedRateWrapper.ConstructorParams memory constructorParams
+    )
+  {
+    require(params.roles.admin != address(0), 'Admin cannot be 0x00');
+    constructorParams.finder = synthereumFinder;
+    constructorParams.version = params.version;
+    constructorParams.pegCollateralToken = params.collateralToken;
+    constructorParams.fixedRateToken = IMintableBurnableERC20(
+      address(tokenCurrency)
+    );
+    constructorParams.roles = params.roles;
+    constructorParams.rate = params.rate;
+  }
+}
