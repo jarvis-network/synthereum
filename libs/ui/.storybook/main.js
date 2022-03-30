8afa996e1d4bfb671e308093c3f575bfecf6fe69
asim_jarvis <asim@jarvis.network>
2022-03-30 16:06:31 +0300

    fix storybook

--- libs/ui/.storybook/main.js
+++ libs/ui/.storybook/main.js
@@ -1,5 +1,9 @@
+const path = require('path');
+const ROOT = path.resolve(__dirname, '../');
+const SRC = `${ROOT}/src`;
+
 module.exports = {
-  stories: ['../src/**/*.stories.tsx'],
+  stories: ['../src/**/*.stories.[tj]sx'],
   addons: [
     '@storybook/preset-create-react-app',
     '@storybook/addon-actions',
@@ -9,10 +13,45 @@ module.exports = {
     '@storybook/addon-backgrounds/register',
     '@storybook/addon-viewport/register',
   ],
-  webpackFinal: config => ({
-    ...config,
-    performance: {
-      hints: false,
-    },
-  }),
+  webpackFinal: async config => {
+    config.module.rules.push(...config.module.rules);
+    config.module.rules.push(
+      {
+        test: /\.tsx?$/,
+        loader: require.resolve('babel-loader'),
+      },
+      {
+        test: /\.stories\.tsx?$/,
+        loaders: [
+          {
+            loader: require.resolve('@storybook/source-loader'),
+            options: { parser: 'typescript' },
+          },
+        ],
+        enforce: 'pre',
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'source-map-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: ['css-loader'],
+      },
+      {
+        test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.eot$/,
+        loader: 'file-loader',
+        options: {
+          outputPath: 'assets',
+          publicPath: 'assets',
+        },
+      },
+    );
+
+    config.resolve.extensions.push('.tsx', '.ts', '.js');
+    config.resolve.modules.push(SRC, 'node_modules');
+
+    return config;
+  },
 };
