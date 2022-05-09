4d67817378da2810b8c9c25cdbed837499a45858
Andrea Di Nenno <dinennoandrea@gmail.com>
2022-05-09 15:51:00 +0200

    test: :white_check_mark: Add marketXyz test setup
    Add marketXyz test setup

--- libs/contracts/test/core/jarvisBrrrrr.js
+++ libs/contracts/test/core/jarvisBrrrrr.js
@@ -13,6 +13,7 @@ const MintableBurnableSyntheticToken = artifacts.require(
 const JarvisBrrrrr = artifacts.require('JarvisBrrrrr');
 const MoneyMarketManager = artifacts.require('MoneyMarketManager');
 const AaveImplementation = artifacts.require('JarvisBrrAave');
+const CompoundImplementation = artifacts.require('JarvisBrrCompound');
 const data = require('../../data/test/lendingTestnet.json');
 
 contract('Jarvis Printer', async accounts => {
@@ -185,7 +186,7 @@ contract('Jarvis Printer', async accounts => {
 
   // these needs to be run on polygon fork and needs exclusive roles
   // skipping it
-  describe('Money market manager - Polygon', () => {
+  describe('Money market manager - Polygonv - AAVEV3', () => {
     let id = 'aave';
     let bytesId = web3.utils.sha3(
       web3.eth.abi.encodeParameters(['string'], [id]),
@@ -194,6 +195,7 @@ contract('Jarvis Printer', async accounts => {
     let jEURPol = '0x4e3decbb3645551b8a19f0ea1678079fcb33fb4c';
     let ajEURPol = '0x6533afac2E7BCCB20dca161449A13A32D391fb00';
     let jEurInst, ajEurInst;
+    let implementationCallArgs = '0x0000';
 
     before(async () => {
       jEurInst = await MintableBurnableSyntheticToken.at(jEURPol);
@@ -265,9 +267,15 @@ contract('Jarvis Printer', async accounts => {
         moneyMarketManager.address,
       );
 
-      let tx = await moneyMarketManager.deposit(jEURPol, amount, id, {
-        from: roles.maintainer,
-      });
+      let tx = await moneyMarketManager.deposit(
+        jEURPol,
+        amount,
+        id,
+        implementationCallArgs,
+        {
+          from: roles.maintainer,
+        },
+      );
       truffleAssert.eventEmitted(tx, 'MintAndDeposit', ev => {
         return (
           ev.token.toLowerCase() == jEURPol.toLowerCase() &&
@@ -305,16 +313,28 @@ contract('Jarvis Printer', async accounts => {
 
       //revert check
       await truffleAssert.reverts(
-        moneyMarketManager.deposit(jEURPol, amount, id, {
-          from: roles.admin,
-        }),
+        moneyMarketManager.deposit(
+          jEURPol,
+          amount,
+          id,
+          implementationCallArgs,
+          {
+            from: roles.admin,
+          },
+        ),
         'Sender must be the maintainer',
       );
 
       await truffleAssert.reverts(
-        moneyMarketManager.deposit(jEURPol, amount, id, {
-          from: accounts[3],
-        }),
+        moneyMarketManager.deposit(
+          jEURPol,
+          amount,
+          id,
+          implementationCallArgs,
+          {
+            from: accounts[3],
+          },
+        ),
         'Sender must be the maintainer',
       );
     });
@@ -334,9 +354,15 @@ contract('Jarvis Printer', async accounts => {
         moneyMarketManager.address,
       );
       let amount = ajEurBalanceBefore.divn(2);
-      let tx = await moneyMarketManager.withdraw(jEURPol, amount, id, {
-        from: roles.maintainer,
-      });
+      let tx = await moneyMarketManager.withdraw(
+        jEURPol,
+        amount,
+        id,
+        implementationCallArgs,
+        {
+          from: roles.maintainer,
+        },
+      );
       let ajEurBalanceAfter = await ajEurInst.balanceOf.call(
         moneyMarketManager.address,
       );
@@ -375,16 +401,28 @@ contract('Jarvis Printer', async accounts => {
 
       //revert check
       await truffleAssert.reverts(
-        moneyMarketManager.withdraw(jEURPol, amount, id, {
-          from: roles.admin,
-        }),
+        moneyMarketManager.withdraw(
+          jEURPol,
+          amount,
+          id,
+          implementationCallArgs,
+          {
+            from: roles.admin,
+          },
+        ),
         'Sender must be the maintainer',
       );
 
       await truffleAssert.reverts(
-        moneyMarketManager.withdraw(jEURPol, amount, id, {
-          from: accounts[3],
-        }),
+        moneyMarketManager.withdraw(
+          jEURPol,
+          amount,
+          id,
+          implementationCallArgs,
+          {
+            from: accounts[3],
+          },
+        ),
         'Sender must be the maintainer',
       );
     });
@@ -401,9 +439,14 @@ contract('Jarvis Printer', async accounts => {
       let ajEurBalanceBefore = await ajEurInst.balanceOf.call(
         moneyMarketManager.address,
       );
-      await moneyMarketManager.withdrawRevenue(jEURPol, id, {
-        from: roles.maintainer,
-      });
+      await moneyMarketManager.withdrawRevenue(
+        jEURPol,
+        id,
+        implementationCallArgs,
+        {
+          from: roles.maintainer,
+        },
+      );
       let ajEurBalanceAfter = await ajEurInst.balanceOf.call(
         moneyMarketManager.address,
       );
@@ -432,6 +475,350 @@ contract('Jarvis Printer', async accounts => {
       assert.equal(
         ajEurBalanceAfter.toString(),
         toBN(ajEurBalanceBefore).sub(expectedRevenue),
+      );
+
+      //revert check
+      await truffleAssert.reverts(
+        moneyMarketManager.withdrawRevenue(
+          jEURPol,
+          id,
+          implementationCallArgs,
+          {
+            from: roles.maintainer,
+          },
+        ),
+        'Sender must be the maintainer',
+      );
+
+      await truffleAssert.reverts(
+        moneyMarketManager.withdrawRevenue(
+          jEURPol,
+          id,
+          implementationCallArgs,
+          {
+            from: roles.maintainer,
+          },
+        ),
+        'Sender must be the maintainer',
+      );
+    });
+  });
+
+  describe('Money market manager - Polygonv - MarketXyz', () => {
+    let id = 'marketxyz';
+    let bytesId = web3.utils.sha3(
+      web3.eth.abi.encodeParameters(['string'], [id]),
+    );
+    let args = '0x0000';
+    let jEURPol = '0x4e3decbb3645551b8a19f0ea1678079fcb33fb4c';
+    let ajEURPol = '0xcfa81742393b52c493b8d76e55ffe4992a5cffd9';
+    let implementationCallArgs = web3.eth.abi.encodeParameters(
+      ['address'],
+      [ajEURPol],
+    );
+
+    let jEurInst, ajEurInst;
+    let implementation;
+    before(async () => {
+      jEurInst = await MintableBurnableSyntheticToken.at(jEURPol);
+      ajEurInst = await MintableBurnableSyntheticToken.at(ajEURPol);
+
+      jarvisBrrrrr = await JarvisBrrrrr.at(
+        '0xb51c6fDbf82eA5C1c2f39e7e6a3f82586C29c81e',
+      );
+
+      // deploy money market manager
+      moneyMarketManager = await MoneyMarketManager.at(
+        '0x9bd4220809e88d4efc1f36365872e688f33cddf0',
+      );
+
+      //deploy aave implementation
+      implementation = await CompoundImplementation.at(
+        '0x37f08323214e8C851e118C4C9Db71321264D1e60',
+      );
+    });
+
+    it('Only maintainer can set a money market implementation', async () => {
+      let tx = await moneyMarketManager.registerMoneyMarketImplementation(
+        id,
+        implementation.address,
+        args,
+        { from: roles.maintainer },
+      );
+      truffleAssert.eventEmitted(tx, 'RegisteredImplementation', ev => {
+        return (
+          ev.id == id &&
+          ev.implementation == implementation.address &&
+          ev.args == args
+        );
+      });
+
+      assert.equal(
+        await moneyMarketManager.idToMoneyMarketImplementation.call(bytesId),
+        implementation.address,
+      );
+      assert.equal(
+        await moneyMarketManager.moneyMarketArgs.call(implementation.address),
+        args,
+      );
+      await truffleAssert.reverts(
+        moneyMarketManager.registerMoneyMarketImplementation(
+          id,
+          implementation.address,
+          args,
+          { from: accounts[5] },
+        ),
+        'Sender must be the maintainer',
+      );
+    });
+
+    it('Only maintainer can mint and deposit into market xyz', async () => {
+      let amount = toWei('1', 'gwei');
+      let depositedSupplyBefore = await moneyMarketManager.moneyMarketBalances.call(
+        bytesId,
+        jEURPol,
+      );
+      let circSupplyBefore = await jarvisBrrrrr.supply.call(jEURPol);
+      let jEurBalanceBefore = await jEurInst.balanceOf.call(
+        moneyMarketManager.address,
+      );
+      let ajEurBalanceBefore = await ajEurInst.balanceOf.call(
+        moneyMarketManager.address,
+      );
+
+      let tx = await moneyMarketManager.deposit(
+        jEURPol,
+        amount,
+        id,
+        implementationCallArgs,
+        {
+          from: roles.maintainer,
+        },
+      );
+      truffleAssert.eventEmitted(tx, 'MintAndDeposit', ev => {
+        return (
+          ev.token.toLowerCase() == jEURPol.toLowerCase() &&
+          ev.moneyMarketId == id &&
+          ev.amount.toString() == amount.toString()
+        );
+      });
+
+      let jEurBalanceAfter = await jEurInst.balanceOf.call(
+        moneyMarketManager.address,
+      );
+      let ajEurBalanceAfter = await ajEurInst.balanceOf.call(
+        moneyMarketManager.address,
+      );
+      let circSupplyAfter = await jarvisBrrrrr.supply.call(jEURPol);
+      let depositedSupplyAfter = await moneyMarketManager.moneyMarketBalances.call(
+        bytesId,
+        jEURPol,
+      );
+
+      assert.equal(
+        depositedSupplyAfter.toString(),
+        toBN(depositedSupplyBefore).add(toBN(amount)).toString(),
+      );
+      assert.equal(
+        circSupplyAfter.toString(),
+        toBN(circSupplyBefore).add(toBN(amount)).toString(),
+      );
+      assert.equal(jEurBalanceAfter.toString(), jEurBalanceBefore.toString());
+
+      let assertion = toBN(ajEurBalanceAfter).gte(
+        toBN(ajEurBalanceBefore).add(toBN(amount)),
+      );
+      assert.equal(assertion, true);
+
+      //revert check
+      await truffleAssert.reverts(
+        moneyMarketManager.deposit(
+          jEURPol,
+          amount,
+          id,
+          implementationCallArgs,
+          {
+            from: roles.admin,
+          },
+        ),
+        'Sender must be the maintainer',
+      );
+
+      await truffleAssert.reverts(
+        moneyMarketManager.deposit(
+          jEURPol,
+          amount,
+          id,
+          implementationCallArgs,
+          {
+            from: accounts[3],
+          },
+        ),
+        'Sender must be the maintainer',
+      );
+    });
+
+    it('Only maintainer can redeem from aave and burn', async () => {
+      let circSupplyBefore = await jarvisBrrrrr.supply.call(jEURPol);
+      let depositedSupplyBefore = await moneyMarketManager.moneyMarketBalances.call(
+        bytesId,
+        jEURPol,
+      );
+
+      let jEurBalanceBefore = await jEurInst.balanceOf.call(
+        moneyMarketManager.address,
+      );
+
+      let ajEurBalanceBefore = await ajEurInst.balanceOf.call(
+        moneyMarketManager.address,
+      );
+      let amount = ajEurBalanceBefore.divn(2);
+      let tx = await moneyMarketManager.withdraw(
+        jEURPol,
+        amount,
+        id,
+        implementationCallArgs,
+        {
+          from: roles.maintainer,
+        },
+      );
+      let ajEurBalanceAfter = await ajEurInst.balanceOf.call(
+        moneyMarketManager.address,
+      );
+      let circSupplyAfter = await jarvisBrrrrr.supply.call(jEURPol);
+      let depositedSupplyAfter = await moneyMarketManager.moneyMarketBalances.call(
+        bytesId,
+        jEURPol,
+      );
+
+      assert.equal(
+        depositedSupplyAfter.toString(),
+        toBN(depositedSupplyBefore).sub(toBN(amount)).toString(),
+      );
+      assert.equal(
+        circSupplyAfter.toString(),
+        toBN(circSupplyBefore).sub(toBN(amount)).toString(),
+      );
+
+      truffleAssert.eventEmitted(tx, 'RedeemAndBurn', ev => {
+        return (
+          ev.token.toLowerCase() == jEURPol.toLowerCase() &&
+          ev.moneyMarketId == id &&
+          ev.amount.toString() == amount.toString()
+        );
+      });
+
+      let jEurBalanceAfter = await jEurInst.balanceOf.call(
+        moneyMarketManager.address,
+      );
+      assert.equal(jEurBalanceAfter.toString(), jEurBalanceBefore.toString());
+
+      let assertion = toBN(ajEurBalanceAfter).gte(
+        toBN(ajEurBalanceBefore).sub(toBN(amount)),
+      );
+      assert.equal(assertion, true);
+
+      //revert check
+      await truffleAssert.reverts(
+        moneyMarketManager.withdraw(
+          jEURPol,
+          amount,
+          id,
+          implementationCallArgs,
+          {
+            from: roles.admin,
+          },
+        ),
+        'Sender must be the maintainer',
+      );
+
+      await truffleAssert.reverts(
+        moneyMarketManager.withdraw(
+          jEURPol,
+          amount,
+          id,
+          implementationCallArgs,
+          {
+            from: accounts[3],
+          },
+        ),
+        'Sender must be the maintainer',
+      );
+    });
+
+    it('Only maintainer can withdraw revenues from deposit', async () => {
+      let depositedSupplyBefore = await moneyMarketManager.moneyMarketBalances.call(
+        bytesId,
+        jEURPol,
+      );
+      let maintainerjEurBalanceBefore = await jEurInst.balanceOf.call(
+        roles.maintainer,
+      );
+
+      let ajEurBalanceBefore = await ajEurInst.balanceOf.call(
+        moneyMarketManager.address,
+      );
+      await moneyMarketManager.withdrawRevenue(
+        jEURPol,
+        id,
+        implementationCallArgs,
+        {
+          from: roles.maintainer,
+        },
+      );
+      let ajEurBalanceAfter = await ajEurInst.balanceOf.call(
+        moneyMarketManager.address,
+      );
+
+      let depositedSupplyAfter = await moneyMarketManager.moneyMarketBalances.call(
+        bytesId,
+        jEURPol,
+      );
+      let maintainerjEurBalanceAfter = await jEurInst.balanceOf.call(
+        roles.maintainer,
+      );
+      let expectedRevenue = toBN(ajEurBalanceBefore).sub(
+        toBN(depositedSupplyBefore),
+      );
+
+      assert.equal(
+        depositedSupplyBefore.toString(),
+        depositedSupplyAfter.toString(),
+      );
+
+      let assertion = maintainerjEurBalanceAfter.gte(
+        toBN(maintainerjEurBalanceBefore).add(toBN(expectedRevenue)),
+      );
+      assert.equal(assertion, true);
+
+      assert.equal(
+        ajEurBalanceAfter.toString(),
+        toBN(ajEurBalanceBefore).sub(expectedRevenue),
+      );
+
+      //revert check
+      await truffleAssert.reverts(
+        moneyMarketManager.withdrawRevenue(
+          jEURPol,
+          id,
+          implementationCallArgs,
+          {
+            from: roles.maintainer,
+          },
+        ),
+        'Sender must be the maintainer',
+      );
+
+      await truffleAssert.reverts(
+        moneyMarketManager.withdrawRevenue(
+          jEURPol,
+          id,
+          implementationCallArgs,
+          {
+            from: roles.maintainer,
+          },
+        ),
+        'Sender must be the maintainer',
       );
     });
   });
