fea77dfce35f92233b4bf29a04db6f0e04bbbd11
Andrea Di Nenno <dinennoandrea@gmail.com>
2022-07-13 17:29:49 +0200

    feat: :art: Extend central bank migration script
    Extend central bank migration script

--- libs/contracts/migrations/17_deploy_centralBank.js
+++ libs/contracts/migrations/17_deploy_centralBank.js
@@ -1,6 +1,9 @@
 module.exports = require('../utils/getContractsFactory')(migrate, [
   'SynthereumFinder',
   'JarvisBrrrrr',
+  'MoneyMarketManager',
+  'JarvisBrrAave',
+  'JarvisBrrCompound',
 ]);
 
 async function migrate(deployer, network, accounts) {
@@ -8,7 +11,13 @@ async function migrate(deployer, network, accounts) {
   const {
     getExistingInstance,
   } = require('@jarvis-network/hardhat-utils/dist/deployment/get-existing-instance');
-  const { SynthereumFinder, JarvisBrrrrr } = migrate.getContracts(artifacts);
+  const {
+    SynthereumFinder,
+    JarvisBrrrrr,
+    MoneyMarketManager,
+    JarvisBrrAave,
+    JarvisBrrCompound,
+  } = migrate.getContracts(artifacts);
   const {
     getKeysForNetwork,
     deploy,
@@ -16,6 +25,7 @@ async function migrate(deployer, network, accounts) {
   const {
     toNetworkId,
   } = require('@jarvis-network/core-utils/dist/eth/networks');
+  const deployData = require('../data/centralBank.json');
 
   const networkId = process.env.FORKCHAINID
     ? process.env.FORKCHAINID
@@ -31,9 +41,9 @@ async function migrate(deployer, network, accounts) {
   const maintainer = process.env.FORKCHAINID
     ? accounts[1]
     : rolesConfig[networkId]?.maintainer ?? accounts[1];
-  const moneyMarketManager = process.env.FORKCHAINID
-    ? accounts[2]
-    : rolesConfig[networkId]?.moneyMarketManager ?? accounts[2];
+  // const moneyMarketManager = process.env.FORKCHAINID
+  //   ? accounts[2]
+  //   : rolesConfig[networkId]?.moneyMarketManager ?? accounts[2];
   const roles = { admin: admin, maintainer: maintainer };
   const keys = getKeysForNetwork(network, accounts);
   await deploy(
@@ -45,6 +55,101 @@ async function migrate(deployer, network, accounts) {
     roles,
     { from: keys.deployer },
   );
+
+  // deploy money market manager
+  await deploy(
+    web3,
+    deployer,
+    network,
+    MoneyMarketManager,
+    synthereumFinder.options.address,
+    roles,
+    { from: keys.deployer },
+  );
+  const moneyMarketManager = await getExistingInstance(
+    web3,
+    MoneyMarketManager,
+    '@jarvis-network/synthereum-contracts',
+  );
+
+  // deploy modules according to json file
+  if (deployData[networkId]?.aave?.deploy ?? false) {
+    await deploy(web3, deployer, network, JarvisBrrAave, {
+      from: keys.deployer,
+    });
+
+    const implementation = await getExistingInstance(
+      web3,
+      JarvisBrrAave,
+      '@jarvis-network/synthereum-contracts',
+    );
+
+    // register implementation
+    let extraArgs = deployData[networkId].aave.extraArgs;
+    await moneyMarketManager.methods
+      .registerMoneyMarketImplementation(
+        'aave',
+        implementation.options.address,
+        extraArgs,
+      )
+      .send({ from: maintainer });
+
+    console.log(
+      `Aave Implementation ${implementation.options.address} registered to money market manager`,
+    );
+  }
+
+  if (deployData[networkId]?.marketxyz?.deploy ?? false) {
+    await deploy(web3, deployer, network, JarvisBrrCompound, {
+      from: keys.deployer,
+    });
+
+    const implementation = await getExistingInstance(
+      web3,
+      JarvisBrrCompound,
+      '@jarvis-network/synthereum-contracts',
+    );
+
+    // register implementation
+    let extraArgs = deployData[networkId].marketxyz.extraArgs;
+    await moneyMarketManager.methods
+      .registerMoneyMarketImplementation(
+        'marketxyz',
+        implementation.options.address,
+        extraArgs,
+      )
+      .send({ from: maintainer });
+
+    console.log(
+      `MarketXyz Implementation ${implementation.options.address} registered to money market manager`,
+    );
+  }
+
+  if (deployData[networkId]?.midas?.deploy ?? false) {
+    await deploy(web3, deployer, network, JarvisBrrCompound, {
+      from: keys.deployer,
+    });
+
+    const implementation = await getExistingInstance(
+      web3,
+      JarvisBrrCompound,
+      '@jarvis-network/synthereum-contracts',
+    );
+
+    // register implementation
+    let extraArgs = deployData[networkId].midas.extraArgs;
+    await moneyMarketManager.methods
+      .registerMoneyMarketImplementation(
+        'midas',
+        implementation.options.address,
+        extraArgs,
+      )
+      .send({ from: maintainer });
+
+    console.log(
+      `Midas Implementation ${implementation.options.address} registered to money market manager`,
+    );
+  }
 
   const jarvisBrrrrrInterface = await web3.utils.stringToHex('JarvisBrrrrr');
   const jarvisBrrrrr = await getExistingInstance(
@@ -66,7 +171,7 @@ async function migrate(deployer, network, accounts) {
   await synthereumFinder.methods
     .changeImplementationAddress(
       moneyMarketMangagerInterface,
-      moneyMarketManager,
+      moneyMarketManager.options.address,
     )
     .send({ from: maintainer });
   console.log('MoneyMarketManager added to SynthereumFinder');
