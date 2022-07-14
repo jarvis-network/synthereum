99a7d0c0bd11093bfa7a577396651f121245d63e
Alessandro Bergamaschi <alessandro@jarvis.network>
2022-07-14 16:38:08 +0200

    test(contracts): Update skip file in coverage settings

--- libs/contracts/test/.solcover.js
+++ libs/contracts/test/.solcover.js
@@ -1,17 +1,4 @@
 module.exports = {
-  skipFiles: [
-    '@chainlink',
-    '@openzeppelin',
-    '@uma',
-    'contracts/test/',
-    'contracts/extensions/',
-    'contracts/base/',
-    'contracts/common/',
-    'contracts/core/',
-    'contracts/fixed-rate/',
-    'contracts/oracle/',
-    'contracts/self-minting/',
-    'contracts/tokens/',
-    'contracts/synthereum-pool/'
-  ],
+  skipFiles: ['@chainlink', '@openzeppelin', '@uma', 'contracts/test/'],
+  measureStatementCoverage: false,
 };
