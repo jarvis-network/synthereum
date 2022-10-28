fa46b17872f05fa424e70986e25d87560e766a9d
Andrea Di Nenno <dinennoandrea@gmail.com>
2022-10-28 12:36:16 +0200

    feat: :art: Add comptroller iface
    Add comptroller iface

--- libs/contracts/contracts/lending-module/interfaces/ICToken.sol
+++ libs/contracts/contracts/lending-module/interfaces/ICToken.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: AGPL-3.0-only
+import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 
 pragma solidity ^0.8.9;
 
-interface ICompoundToken {
+interface ICompoundToken is IERC20 {
   function mintBehalf(address, uint256) external returns (uint256);
 
   function exchangeRateCurrent() external returns (uint256);
@@ -15,10 +16,6 @@ interface ICompoundToken {
 
   function balanceOfUnderlying(address owner) external returns (uint256);
 
-  function balanceOf(address owner) external view returns (uint256);
-
-  function name() external view returns (string memory);
-
   function getAccountSnapshot(address account)
     external
     view
@@ -28,4 +25,10 @@ interface ICompoundToken {
       uint256,
       uint256
     );
+
+  function underlying() external view returns (address);
+}
+
+interface IComptroller {
+  function getAllMarkets() external view returns (address[] memory);
 }
