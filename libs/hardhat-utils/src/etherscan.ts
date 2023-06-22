87c6a1b5537c7d5e6b8b1619dfe644b270d383b6
Alessandro Bergamaschi <alessandro@jarvis.network>
2023-06-22 17:27:57 +0200

    feat(hardhat-utils): Upgrade verision of hardhat-etherscan for verification

--- libs/hardhat-utils/src/etherscan.ts
+++ libs/hardhat-utils/src/etherscan.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { HardhatUserConfig } from 'hardhat/types';
 
 import '@nomiclabs/hardhat-etherscan';
@@ -5,20 +6,20 @@ import '@nomiclabs/hardhat-etherscan';
 export function addEtherscanApiKeys(config: HardhatUserConfig) {
   config.etherscan = {
     apiKey: {
-      mainnet: process.env.ETHERSCAN_ETH_API_KEY,
-      kovan: process.env.ETHERSCAN_ETH_API_KEY,
-      ropsten: process.env.ETHERSCAN_ETH_API_KEY,
-      polygon: process.env.ETHERSCAN_POLYGON_API_KEY,
-      polygonMumbai: process.env.ETHERSCAN_POLYGON_API_KEY,
-      bsc: process.env.ETHERSCAN_BSC_API_KEY,
-      bscTestnet: process.env.ETHERSCAN_BSC_API_KEY,
-      opera: process.env.ETHERSCAN_FANTOM_API_KEY,
-      ftmTestnet: process.env.ETHERSCAN_FANTOM_API_KEY,
-      xdai: process.env.BLOCKSCOUT_XDAI_API_KEY,
-      sokol: process.env.BLOCKSCOUT_XDAI_API_KEY,
-      avalancheFujiTestnet: process.env.ETHERSCAN_AVALANCHE_API_KEY,
-      avalanche: process.env.ETHERSCAN_AVALANCHE_API_KEY,
-      optimisticEthereum: process.env.OPTIMISM_API_KEY,
+      mainnet: process.env.ETHERSCAN_ETH_API_KEY!,
+      kovan: process.env.ETHERSCAN_ETH_API_KEY!,
+      ropsten: process.env.ETHERSCAN_ETH_API_KEY!,
+      polygon: process.env.ETHERSCAN_POLYGON_API_KEY!,
+      polygonMumbai: process.env.ETHERSCAN_POLYGON_API_KEY!,
+      bsc: process.env.ETHERSCAN_BSC_API_KEY!,
+      bscTestnet: process.env.ETHERSCAN_BSC_API_KEY!,
+      opera: process.env.ETHERSCAN_FANTOM_API_KEY!,
+      ftmTestnet: process.env.ETHERSCAN_FANTOM_API_KEY!,
+      xdai: process.env.BLOCKSCOUT_XDAI_API_KEY!,
+      sokol: process.env.BLOCKSCOUT_XDAI_API_KEY!,
+      avalancheFujiTestnet: process.env.ETHERSCAN_AVALANCHE_API_KEY!,
+      avalanche: process.env.ETHERSCAN_AVALANCHE_API_KEY!,
+      optimisticEthereum: process.env.OPTIMISM_API_KEY!,
     },
   };
 }
